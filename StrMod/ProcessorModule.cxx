b63ac44cc1f1b1d4a8b6622f4da1a955f5b4a3e2
Eric Hopper <hopper@omnifarious.org>
1996-09-02 23:31:20 +0000

    Added class that would turn two StreamProcessors into a StreamModule that used one StreamProcessor for the data flowing in each direction.

---
+++ StrMod/ProcessorModule.cxx
@@ -0,0 +1,130 @@
+/* $Header$ */
+
+// $Log$
+// Revision 1.1  1996/09/02 23:31:12  hopper
+// Added class that would turn two StreamProcessors into a StreamModule
+// that used one StreamProcessor for the data flowing in each direction.
+//
+
+#ifdef __GNUG__
+#  pragma implementation "ProcessorModule.h"
+#endif
+
+#include <StrMod/ProcessorModule.h>
+#include <iostream.h>
+
+const STR_ClassIdent ProcessorModule::identifier(29UL);
+const STR_ClassIdent ProcessorModule::Plug::identifier(30UL);
+
+ProcessorModule::ProcessorModule(StreamProcessor *one, StreamProcessor *other)
+     : side_(this, otherside_, OneSide, one, other),
+       otherside_(this, side_, OtherSide, other, one)
+{
+   pulled_.side = false;
+   pulled_.otherside = false;
+}
+
+ProcessorModule::~ProcessorModule()
+{
+   if (side_.PluggedInto()) {
+      side_.UnPlug();
+   }
+   if (otherside_.PluggedInto()) {
+      otherside_.UnPlug();
+   }
+}
+
+bool ProcessorModule::DeletePlug(StrPlug *plug)
+{
+   const StrPlug * const sidep = &side_;
+   const StrPlug * const othersidep = &otherside_;
+
+   if (plug == sidep && pulled_.side) {
+      side_.UnPlug();
+      pulled_.side = false;
+      return(true);
+   } else if (plug == othersidep && pulled_.otherside) {
+      otherside_.UnPlug();
+      pulled_.otherside = false;
+      return(true);
+   }
+   return(false);
+}
+
+StrPlug *ProcessorModule::CreatePlug(int side)
+{
+   assert((side == OneSide) || (side == OtherSide));
+
+   PlugSide rside = static_cast<PlugSide>(side);
+
+   switch (rside) {
+    case OneSide:
+      if (!pulled_.side) {
+	 return(&side_);
+      }
+      break;
+    case OtherSide:
+      if (!pulled_.otherside) {
+	 return(&otherside_);
+      }
+      break;
+   }
+   return(0);
+}
+
+bool ProcessorModule::CanCreate(int side) const
+{
+   switch (side) {
+    case OneSide:
+      return(!pulled_.side);
+      break;
+    case OtherSide:
+      return(!pulled_.otherside);
+      break;
+   }
+   return(false);
+}
+
+bool ProcessorModule::Plug::i_Write(const StrChunkPtr &chnk)
+{
+   assert(chnk);
+   cerr << "ProcessorModule::Plug::i_Write called ("
+	<< (Side() == OneSide ? "OneSide" :
+	    (Side() == OtherSide ? "OtherSide" : "Unknown")) << ")\n";
+
+   bool oldoutgoing = inproc_->CanPullOutgoing();
+   inproc_->AddIncoming(chnk);
+   if (!oldoutgoing && inproc_->CanPullOutgoing()) {
+      otherplug_.CheckNotify();
+   }
+
+   return(true);
+}
+
+const StrChunkPtr ProcessorModule::Plug::i_InternalRead()
+{
+//   cerr << "ProcessorModule::Plug::i_InternalRead called!\n";
+
+   bool oldincoming = outproc_->CanAddIncoming();
+   StrChunkPtr chnk = outproc_->PullOutgoing();
+
+   if (!oldincoming && outproc_->CanAddIncoming()) {
+      otherplug_.CheckNotify();
+   }
+
+   return(chnk);
+}
+
+void ProcessorModule::Plug::CheckNotify()
+{
+//   cerr << "ProcessorModule::Plug::CheckNotify called!\n";
+   if (CanWrite()) {
+//      cerr << "ProcessorModule::Plug::DoWriteableNotify called!\n";
+      DoWriteableNotify();
+   }
+   if (CanRead()) {
+//      cerr << "ProcessorModule::Plug::DoReadableNotify called!\n";
+      DoReadableNotify();
+   }
+}
+
---
+++ StrMod/StrMod/ProcessorModule.h
@@ -0,0 +1,148 @@
+#ifndef _STR_ProcessorModule_H_  // -*-c++-*-
+
+#ifdef __GNUG__
+#  pragma interface
+#endif
+
+/* $Header$ */
+
+// $Log$
+// Revision 1.1  1996/09/02 23:31:20  hopper
+// Added class that would turn two StreamProcessors into a StreamModule
+// that used one StreamProcessor for the data flowing in each direction.
+//
+
+#include <StrMod/STR_ClassIdent.h>
+#include <StrMod/StreamModule.h>
+#include <StrMod/StreamProcessor.h>
+#include <StrMod/SimplePlug.h>
+
+#define _STR_ProcessorModule_H_
+
+class StrChunk;
+
+class ProcessorModule : public StreamModule {
+ public:
+   static const STR_ClassIdent identifier;
+
+   enum PlugSide { OneSide, OtherSide };
+   class Plug : public SimplePlugOf<ProcessorModule> {
+      friend class ProcessorModule;
+
+    public:
+      typedef SimplePlugOf<ProcessorModule> parentclass;
+
+      static const STR_ClassIdent identifier;
+
+      inline virtual int AreYouA(const ClassIdent &cid) const;
+
+      inline virtual int Side() const;
+
+    protected:
+      virtual const ClassIdent *i_GetIdent() const     { return(&identifier); }
+
+      inline virtual bool i_CanWrite() const;
+      virtual bool i_Write(const StrChunkPtr &);
+      inline virtual bool i_CanRead() const;
+      virtual const StrChunkPtr i_InternalRead();
+
+   private:
+      Plug &otherplug_;
+      PlugSide side_;
+      StreamProcessor * const inproc_;
+      StreamProcessor * const outproc_;
+
+      inline Plug(ProcessorModule *prnt, Plug &otherplug, PlugSide side,
+		  StreamProcessor *inproc, StreamProcessor *outproc);
+      virtual ~Plug()                                  { }
+
+      void CheckNotify();
+   };
+
+   ProcessorModule(StreamProcessor *in, StreamProcessor *out);
+   virtual ~ProcessorModule();
+
+   inline virtual int AreYouA(const ClassIdent &cid) const;
+
+   inline bool CanCreate(PlugSide side) const;
+   inline Plug *MakePlug(PlugSide side);
+   inline virtual bool OwnsPlug(StrPlug *plug) const;
+   virtual bool DeletePlug(StrPlug *plug);
+
+ protected:
+   virtual const ClassIdent *i_GetIdent() const        { return(&identifier); }
+
+   virtual StrPlug *CreatePlug(int side);
+
+   virtual bool CanCreate(int side) const;
+
+ private:
+   Plug side_, otherside_;
+   struct {
+      bool side : 1;
+      bool otherside : 1;
+   } pulled_;
+};
+
+//-----------------------------inline functions--------------------------------
+
+//----------ProcessorModule::Plug inlines-----
+
+inline int ProcessorModule::Plug::AreYouA(const ClassIdent &cid) const
+{
+   return((identifier == cid) || parentclass::AreYouA(cid));
+}
+
+inline bool ProcessorModule::Plug::i_CanWrite() const
+{
+   return(inproc_->CanAddIncoming());
+}
+
+inline bool ProcessorModule::Plug::i_CanRead() const
+{
+   return(outproc_->CanPullOutgoing());
+}
+
+inline int ProcessorModule::Plug::Side() const
+{
+   return(side_);
+}
+
+inline ProcessorModule::Plug::Plug(ProcessorModule *prnt, Plug &other,
+				   PlugSide side,
+				   StreamProcessor *i, StreamProcessor *o)
+     : parentclass(prnt), otherplug_(other),
+       side_(side), inproc_(i), outproc_(o)
+{
+}
+
+//----------ProcessorModule inlines-----
+
+inline int ProcessorModule::AreYouA(const ClassIdent &cid) const
+{
+   return((identifier == cid) || StreamModule::AreYouA(cid));
+}
+
+inline bool ProcessorModule::CanCreate(PlugSide side) const
+{
+   return((side == OneSide) ? !pulled_.side : !pulled_.otherside);
+}
+
+inline ProcessorModule::Plug *ProcessorModule::MakePlug(PlugSide side)
+{
+   StrPlug *tmp;
+
+   tmp = CreatePlug(side);
+   return(static_cast<ProcessorModule::Plug *>(tmp));
+}
+
+inline bool ProcessorModule::OwnsPlug(StrPlug *plug) const
+{
+   const StrPlug * const sidep = &side_;
+   const StrPlug * const othersidep = &otherside_;
+
+   return((plug == sidep && pulled_.side) ||
+	  (plug == othersidep && pulled_.otherside));
+}
+
+#endif
