89bf2e18a51ef441b98d2a75ca108ee16373430f
Eric Hopper <hopper@omnifarious.org>
1996-09-02 23:18:48 +0000

    Added NewlineChopper class so users would have a simple class that would break up and recombine streams using '\n' as a separator.

---
+++ StrMod/NewlineChopper.cxx
@@ -0,0 +1,15 @@
+/* $Header$ */
+
+// $Log$
+// Revision 1.1  1996/09/02 23:18:40  hopper
+// Added NewlineChopper class so users would have a simple class that
+// would break up and recombine streams using '\n' as a separator.
+//
+
+#ifdef __GNUG__
+#  pragma implementation "NewlineChopper.h"
+#endif
+
+#include <StrMod/NewlineChopper.h>
+
+const STR_ClassIdent NewlineChopper::identifier(27UL);
---
+++ StrMod/StrMod/NewlineChopper.h
@@ -0,0 +1,43 @@
+#ifndef _STR_NewlineChopper_H_  // -*-c++-*-
+
+#ifdef __GNUG__
+#  pragma interface
+#endif
+
+/* $Header$ */
+
+// $Log$
+// Revision 1.1  1996/09/02 23:18:48  hopper
+// Added NewlineChopper class so users would have a simple class that
+// would break up and recombine streams using '\n' as a separator.
+//
+
+#include <StrMod/CharChopper.h>
+
+#define _STR_NewlineChopper_H_
+
+class NewlineChopper : public CharChopper {
+ public:
+   typedef CharChopper parentclass;
+   static const STR_ClassIdent identifier;
+
+   NewlineChopper() : CharChopper('\n')                { }
+   // Derived class destructor doesn't do anything base class one doesn't do.
+
+   inline virtual int AreYouA(const ClassIdent &cid) const;
+
+ protected:
+   virtual const ClassIdent *i_GetIdent() const        { return(&identifier); }
+
+ private:
+   NewlineChopper(const NewlineChopper &b);
+};
+
+//-----------------------------inline functions--------------------------------
+
+inline int NewlineChopper::AreYouA(const ClassIdent &cid) const
+{
+   return((identifier == cid) || parentclass::AreYouA(cid));
+}
+
+#endif
