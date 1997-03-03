74b8b429234abf71a5e5dd0694ef235fedc85289
Eric Hopper <hopper@omnifarious.org>
1997-03-03 01:49:01 +0000

    Skeleton only

---
+++ UniEvent/UniEvent/Dispatcher.h
@@ -0,0 +1,16 @@
+#ifndef _UNEVT_Dispatcher_H_  // -*-c++-*-
+
+#ifdef __GNUG__
+#  pragma interface
+#endif
+
+/* $Header$ */
+
+// $Log$
+// Revision 1.1  1997/03/03 01:49:00  hopper
+// Skeleton only
+//
+
+#define _UNEVT_Dispatcher_H_
+
+#endif
---
+++ UniEvent/UniEvent/Event.h
@@ -0,0 +1,16 @@
+#ifndef _UNEVT_Event_H_  // -*-c++-*-
+
+#ifdef __GNUG__
+#  pragma interface
+#endif
+
+/* $Header$ */
+
+// $Log$
+// Revision 1.1  1997/03/03 01:49:01  hopper
+// Skeleton only
+//
+
+#define _UNEVT_Event_H_
+
+#endif
