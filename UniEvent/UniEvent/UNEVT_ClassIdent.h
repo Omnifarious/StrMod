8d74ee409423ceda94c8aa02d7693f1ba992241d
Eric Hopper <hopper@omnifarious.org>
1997-03-03 05:24:19 +0000

    Gensis!

---
+++ UniEvent/UniEvent/UNEVT_ClassIdent.h
@@ -0,0 +1,51 @@
+#ifndef _UNEVT_UNEVT_ClassIdent_H_
+
+#ifdef __GNUG__
+#  pragma interface
+#endif
+
+/* $Header$ */
+
+// For log see ../ChangeLog
+
+#include <LCore/HopClTypes.h>
+
+#define _UNEVT_UNEVT_ClassIdent_H_
+
+class UNEVT_ClassIdent : public EH_ClassIdent {
+ protected:
+   inline virtual const ClassIdent *i_GetIdent() const;
+
+ public:
+   static const UNEVT_ClassIdent identifier;
+
+   inline virtual int AreYouA(const ClassIdent &cid) const;
+
+   inline bool operator ==(const ClassIdent &b) const;
+
+   inline UNEVT_ClassIdent(U4Byte cnum);
+};
+
+//---------------------------inline functions--------------------------------
+
+inline const ClassIdent *UNEVT_ClassIdent::i_GetIdent() const
+{
+   return(&identifier);
+}
+
+inline int UNEVT_ClassIdent::AreYouA(const ClassIdent &cid) const
+{
+   return((identifier == cid) || EH_ClassIdent::AreYouA(cid));
+}
+
+inline bool UNEVT_ClassIdent::operator ==(const ClassIdent &b) const
+{
+   return(EH_ClassIdent::operator ==(b));
+}
+
+inline UNEVT_ClassIdent::UNEVT_ClassIdent(U4Byte cnum) :
+     EH_ClassIdent(EH_ClassNum(EH_ClassNum::User3, cnum))
+{
+}
+
+#endif
