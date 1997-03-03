f3a7f6f5c02535f6f56b6ea95b1d761599f66e94
Eric Hopper <hopper@omnifarious.org>
1997-03-03 05:19:26 +0000

    First cut at definition for abstract UNIDispatcher class.

--- UniEvent/UniEvent/Dispatcher.h
+++ UniEvent/UniEvent/Dispatcher.h
@@ -6,11 +6,49 @@
 
 /* $Header$ */
 
-// $Log$
-// Revision 1.1  1997/03/03 01:49:00  hopper
-// Skeleton only
-//
+// For log see ../ChangeLog
+
+#include <LCore/Protocol.h>
+#include <bool.h>
+#include <UnEvt/UNEVT_ClassIdent.h>
 
 #define _UNEVT_Dispatcher_H_
+
+class UNIEvent;
+class OSConditionManager;
+
+class UNIDispatcher : virtual public Protocol {
+ public:
+   static const UnEvt_ClassIdent identifier;
+
+   virtual ~UNIDispatcher()                            { }
+
+   virtual int AreYouA(const ClassIdent &cid) const;
+
+   virtual void AddEvent(UNIEvent *ev) = 0;
+
+   void DispatchEvent()                                { DispatchEvents(1); }
+   virtual void DispatchEvents(unsigned int numevents) = 0;
+   virtual void DispatchUntilEmpty() = 0;
+   virtual void StopDispatching() = 0;
+   virtual bool_ret IsQueueEmpty() const = 0;
+
+   virtual OSConditionManager *GetConditionManager()   { return(0); }
+
+ protected:
+   virtual const ClassIdent *i_GetIdent() const        { return(&identifier); }
+
+ private:
+   // Purposely left undefined.
+   UNIDispatcher(const UNIDispatcher &b);
+   const UNIDispatcher &operator =(const UNIDispatcher &b);
+};
+
+//-----------------------------inline functions--------------------------------
+
+virtual int AreYouA(const ClassIdent &cid) const;
+{
+   return((identifier == cid) || Protocol::AreYouA(cid));
+}
 
 #endif
