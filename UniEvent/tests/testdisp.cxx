cf3bebff227a410ac6f4aedbb9261b48d2f782b0
Eric Hopper <hopper@omnifarious.org>
1997-05-13 01:07:25 +0000

    Changed test to use new EventPtr classes, and also do construction and destruction counting to make sure reference counting is working.

--- UniEvent/tests/testdisp.cxx
+++ UniEvent/tests/testdisp.cxx
@@ -4,29 +4,48 @@
 
 #include "UniEvent/Dispatcher.h"
 #include "UniEvent/Event.h"
+#include "UniEvent/EventPtr.h"
 #include "testdisp.h"
 #include <stdlib.h>
 #include <iostream.h>
 
 class CountingEvent : public UNIEvent {
  public:
-   CountingEvent()                                  { this_count_ = count_++; }
-   virtual ~CountingEvent()                         { }
+   inline CountingEvent();
+   virtual ~CountingEvent();
 
    virtual void TriggerEvent(UNIDispatcher *dispatcher = 0);
 
-   static unsigned int GetCount()                   { return(count_); }
-   static void SetCount(unsigned int ct)            { count_ = ct; }
+   static unsigned int GetCount()                      { return(count_); }
+   static void SetCount(unsigned int ct)               { count_ = ct; }
+
+   static unsigned int GetMakeCount()                  { return(make_count_); }
+   static unsigned int GetKillCount()                  { return(kill_count_); }
 
  protected:
-   unsigned int MyCount() const                     { return(this_count_); }
+   unsigned int MyCount() const                        { return(this_count_); }
 
  private:
    static unsigned int count_;
+   static unsigned int make_count_;
+   static unsigned int kill_count_;
    unsigned int this_count_;
 };
 
 unsigned int CountingEvent::count_ = 0;
+unsigned int CountingEvent::make_count_ = 0;
+unsigned int CountingEvent::kill_count_ = 0;
+
+inline CountingEvent::CountingEvent()
+{
+   this_count_ = count_++;
+   make_count_++;
+}
+
+CountingEvent::~CountingEvent()
+{
+   kill_count_++;
+}
 
 void CountingEvent::TriggerEvent(UNIDispatcher *dispatcher)
 {
@@ -73,7 +92,6 @@ void GrowQueueEvent::TriggerEvent(UNIDispatcher *dispatcher)
 	 }
       }
    }
-   delete this;
 }
 
 void ShrinkQueueEvent::TriggerEvent(UNIDispatcher *dispatcher)
@@ -90,7 +108,6 @@ void ShrinkQueueEvent::TriggerEvent(UNIDispatcher *dispatcher)
 	 dispatcher->AddEvent(new ShrinkQueueEvent);
       }
    }
-   delete this;
 }
 
 bool_val TestDispatcher(UNIDispatcher &disp, long seed)
@@ -102,5 +119,8 @@ bool_val TestDispatcher(UNIDispatcher &disp, long seed)
       disp.AddEvent(new GrowQueueEvent);
    }
    disp.DispatchUntilEmpty();
+   cout << " Count == " << CountingEvent::GetCount() << " && "
+	<< "Make count == " << CountingEvent::GetMakeCount() << " && "
+	<< "Kill count == " << CountingEvent::GetKillCount() << endl;
    return(true);
 }
