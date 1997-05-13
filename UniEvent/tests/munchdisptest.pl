b6cd27fd9cbe775ef56f9fdb50932b3ad5ee611a
Eric Hopper <hopper@omnifarious.org>
1997-05-13 01:07:01 +0000

    Changed to print out more standard success and failure messages, and to check the new construction and destruction counts.

--- UniEvent/tests/munchdisptest.pl
+++ UniEvent/tests/munchdisptest.pl
@@ -8,9 +8,24 @@ $startnum = <>;
 $startnum =~ s/^[^0-9]*([0-9]*)([^0-9]*)?$/$1/;
 
 while ($curnum = <>) {
-    $curnum =~ s/^[^0-9]*([0-9]*)([^0-9]*)?$/$1/;
-    if ($curnum != ($startnum + 1)) {
-	print STDERR "SKIP: $startnum->$curnum\n";
+    if ($curnum =~ /^Event/) {
+	$curnum =~ s/^[^0-9]*([0-9]*)([^0-9]*)?$/$1/;
+	if ($curnum != ($startnum + 1)) {
+	    print "ERROR: skip $startnum->$curnum\n";
+	}
+	$startnum = $curnum;
+    } elsif ($curnum =~ /^ Count/) {
+	$curnum =~ /^[^0-9]*([0-9]+)[^0-9]+([0-9]+)[^0-9]+([0-9]+)[^0-9]*$/;
+	($n1, $n2, $n3) = ($1, $2, $3);
+	if ($n1 != ($startnum + 1)) {
+	    print "ERROR: Wrong ending value: $n1\n";
+	} else {
+	    print "OK: $n1 ", $startnum + 1, "\n";
+	}
+	if (($n1 != $n2) || ($n2 != $n3)) {
+	    print "ERROR: Ending numbers don't match: $n1 $n2 $n3\n";
+	} else {
+	    print "OK: $n1 $n2 $n3\n";
+	}
     }
-    $startnum = $curnum;
 }
