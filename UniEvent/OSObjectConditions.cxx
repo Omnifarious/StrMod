86201dc3585461bdc79982feedabcc9ab6519994
Eric Hopper <hopper@omnifarious.org>
1997-05-18 23:31:24 +0000

    Added to make place for inline member functions and identifier constant to be.

---
+++ UniEvent/OSObjectConditions.cxx
@@ -0,0 +1,11 @@
+/* $Header$ */
+
+// Log can be found in ChangeLog
+
+#ifdef __GNUG__
+#  pragma implementation "OSObjectConditions.h"
+#endif
+
+#include <UniEvent/OSObjectConditions.h>
+
+const UNEVT_ClassIdent OSObjectConditions::identifier(6UL);
