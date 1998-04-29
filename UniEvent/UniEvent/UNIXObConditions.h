c985d3bb5204ee5e20e07849f9eb13866b43ca44
Eric Hopper <hopper@omnifarious.org>
1998-04-29 01:59:58 +0000

    These are an attempt at a much more complex and unified architecture.  For now, they are not being used at all, but I may want to bring them back someday.

---
+++ UniEvent/UniEvent/UNIXObConditions.h
@@ -0,0 +1,22 @@
+#ifndef _UNEVT_UNIXObConditions_H_  // -*-c++-*-
+
+#ifdef __GNUG__
+#  pragma interface
+#endif
+
+/* $Header$ */
+
+// $Log$
+// Revision 1.1  1998/04/29 01:59:58  hopper
+// These are an attempt at a much more complex and unified architecture.  For
+// now, they are not being used at all, but I may want to bring them back
+// someday.
+//
+
+#include <UniEvent/StandObConditions.h>
+
+#define _UNEVT_UNIXObConditions_H_
+
+class UNIXConditionManager : public 
+
+#endif
