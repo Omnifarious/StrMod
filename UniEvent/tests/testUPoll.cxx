90faaad04070954c21dceadbfefa290400c04557
Eric Hopper <hopper@omnifarious.org>
1998-05-01 01:14:37 +0000

    Added a new test for the UNIXpollManager.

---
+++ UniEvent/tests/testUPoll.cxx
@@ -0,0 +1,133 @@
+/* $Header$ */
+
+// $Log$
+// Revision 1.1  1998/05/01 01:14:37  hopper
+// Added a new test for the UNIXpollManager.
+//
+
+#include "UniEvent/SimpleDispatcher.h"
+#include "UniEvent/UNIXpollManagerImp.h"
+#include "UniEvent/EventPtrT.h"
+#include <iostream.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+
+class MyEvent : public UNIXpollManager::PollEvent {
+ public:
+   inline MyEvent(int fd, UNIXpollManager &pollman);
+   virtual ~MyEvent();
+
+   virtual void TriggerEvent(UNIDispatcher *dispatcher);
+
+ private:
+   int fd_;
+   unsigned int counter_;
+   char strbuf_[80];
+   size_t bufpos_, buflen_;
+   UNIXpollManager &pollman_;
+};
+
+inline MyEvent::MyEvent(int fd, UNIXpollManager &pollman)
+     : fd_(fd), pollman_(pollman), bufpos_(0), buflen_(0), counter_(0)
+{
+   sprintf(strbuf_, "%d:%ud\n", fd_, counter_);
+   buflen_ = strlen(strbuf_);
+   cerr << "Created MyEvent(" << fd_ << ")\n";
+}
+
+MyEvent::~MyEvent()
+{
+   cerr << "Destroying MyEvent(" << fd_
+	<< ")  (counter_ == " << counter_ << ")\n";
+}
+
+void MyEvent::TriggerEvent(UNIDispatcher *dispatcher)
+{
+   unsigned int condbits = getCondBits();
+   unsigned int newcondbits = 0;
+
+   cerr << "MyEvent("<< fd_ << ")::TriggerEvent\n";
+
+   if (condbits & UNIXpollManager::FD_Writeable)
+   {
+      int result;
+
+      result = write(fd_, strbuf_ + bufpos_, buflen_ - bufpos_);
+      while (result > 0 && (counter_ < 8192))
+      {
+	 bufpos_ += result;
+	 if (bufpos_ >= buflen_)
+	 {
+	    sprintf(strbuf_, "%d:%u\n", fd_, ++counter_);
+	    buflen_ = strlen(strbuf_);
+	    bufpos_ = 0;
+	 }
+	 result = write(fd_, strbuf_ + bufpos_, buflen_ - bufpos_);
+      }
+      if (result < 0)
+      {
+	 if (errno == EAGAIN)
+	 {
+	    newcondbits |= UNIXpollManager::FD_Writeable;
+	 }
+	 else
+	 {
+	    condbits |= UNIXpollManager::FD_Error;
+	 }
+      }
+      else
+      {
+	 cerr << "MyEvent(" << fd_ << ") done!\n";
+	 close(fd_);
+      }
+   }
+   else if (condbits & ~UNIXpollManager::FD_Writeable)
+   {
+      int errnocapture = errno;
+
+      cerr << "MyEvent(" << fd_ << ") some weird error happened.\n";
+      cerr << "condbits == " << condbits << "\n";
+      cerr << "errno == " << errnocapture << "\n";
+   }
+   if (newcondbits)
+   {
+      newcondbits |=
+	 UNIXpollManager::FD_Error
+	 | UNIXpollManager::FD_Closed
+	 | UNIXpollManager::FD_Invalid;
+      pollman_.registerFDCond(fd_, newcondbits, UNIEventPtrT<PollEvent>(this));
+   }
+}
+
+int main(int argc, char *argv[])
+{
+   UNISimpleDispatcher disp;
+   UNIXpollManagerImp upm(&disp);
+   unsigned int defaultcond = 0;
+
+   defaultcond |=
+      UNIXpollManager::FD_Writeable
+      | UNIXpollManager::FD_Error
+      | UNIXpollManager::FD_Closed
+      | UNIXpollManager::FD_Invalid;
+   for (int i = 1; i < argc; ++i)
+   {
+      int fd = open(argv[i], O_WRONLY | O_NONBLOCK);
+
+      if (fd < 0)
+      {
+	 cerr << "Unable to open " << argv[i]
+	      << " for non-blocking writing.\n";
+      }
+      else
+      {
+	 upm.registerFDCond(fd, defaultcond,
+			    UNIEventPtrT<UNIXpollManager::PollEvent>(new MyEvent(fd, upm)));
+      }
+   }
+   disp.DispatchUntilEmpty();
+   return(0);
+}
