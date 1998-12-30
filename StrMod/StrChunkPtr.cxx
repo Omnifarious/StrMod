dff83d4aded20963bf475455da9855a2f7d2d10a
Eric Hopper <hopper@omnifarious.org>
1998-12-30 20:32:29 +0000

    Made log comment point to ChangeLog file.

--- StrMod/StrChunkPtr.cxx
+++ StrMod/StrChunkPtr.cxx
@@ -1,13 +1,6 @@
 /* $Header$ */
 
-// $Log$
-// Revision 1.3  1998/12/30 20:31:51  hopper
-// Changed the whole face of the StrChunkPtr class to be derived from
-// RefCountPtrT<StrChunk> because with current compiler has rendered this a
-// safe thing to do, and it more accurately reflects semantics anyway.  As
-// part of this, StrChunk has also been changed to derive from
-// ReferenceCounting, because that is also more consistent with semantics,
-// and allows the previously mentioned change.
+// For log see ChangeLog
 //
 // Revision 1.2  1996/07/05 19:46:33  hopper
 // Various changed to make implementing StrChunkPtrT template easier and
