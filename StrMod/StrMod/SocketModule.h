089ecf885b57205d490ccdb7afa7e58a1e0551dc
Eric Hopper <hopper@omnifarious.org>
1999-01-14 11:37:18 +0000

    Added support for writing an EOF indication to a socket. Made data private that should've been in the first place.

--- StrMod/StrMod/SocketModule.h
+++ StrMod/StrMod/SocketModule.h
@@ -34,8 +34,12 @@ class SocketModule : public StreamFDModule {
    const SocketAddress &GetPeerAddr()                  { return(peer); }
 
  protected:
-   SocketAddress &peer;
-   int makesock_errno;
+   //: An EOF indication has been written.  This function has to handle it.
+   // This function follows the template method pattern from Design Patterns.
+   // Return true if the fd can now be written to, and return false if it
+   // can't.  It would probably also be best to set some kind of error
+   // condition if it can't be written to.
+   virtual bool writeEOF();
 
    //: Create a SocketModule using the given fd
    // Note that ownership of pr is being passed.
@@ -45,6 +49,10 @@ class SocketModule : public StreamFDModule {
 
    static int MakeSocket(SocketModule &obj,
 			 const SocketAddress &addr, bool blockconnect);
+
+ private:
+   SocketAddress &peer;
+   int makesock_errno;
 };
 
 //-------------------------------inline functions------------------------------
