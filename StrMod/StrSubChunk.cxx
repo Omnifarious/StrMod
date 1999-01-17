240102fc0953a78bf859f73e669aa4adbd5f5778
Eric Hopper <hopper@omnifarious.org>
1999-01-17 18:32:05 +0000

    Changed to automatically make a StrSubChunk of a StrSubChunk into just a StrSubChunk.

--- StrMod/StrSubChunk.cxx
+++ StrMod/StrSubChunk.cxx
@@ -1,6 +1,10 @@
 /* $Header$ */
 
 // $Log$
+// Revision 1.2  1999/01/17 18:32:05  hopper
+// Changed to automatically make a StrSubChunk of a StrSubChunk into just a
+// StrSubChunk.
+//
 // Revision 1.1  1996/06/29 06:57:44  hopper
 // New class StrSubChunk used to make a part of an existing chunk look like
 // a full chunk to the outside world.
@@ -11,6 +15,8 @@
 #endif
 
 #include "StrMod/StrSubChunk.h"
+#include "StrMod/StrChunkPtrT.h"
+#include "StrMod/StrChunkPtr.h"
 #include <assert.h>
 // #include <iostream.h>
 
@@ -21,14 +27,25 @@ StrSubChunk::StrSubChunk(const StrChunkPtr &chunk, const LinearExtent &extent)
 {
    assert(subchunk_);
 
-   unsigned int sublen = subchunk_->Length();
+   if (subchunk_->AreYouA(identifier))
+   {
+      StrChunkPtrT<StrSubChunk> subc
+	 = static_cast<StrSubChunk *>(subchunk_.GetPtr());
 
-   if ((subext_.Offset() + subext_.Length()) > sublen) {
-      LinearExtent chunkext(0, sublen);
+      subext_ = subc->subext_.SubExtent(subext_);
+      subchunk_ = subc->subchunk_;
+   }
+   else
+   {
+      unsigned int sublen = subchunk_->Length();
+
+      if ((subext_.Offset() + subext_.Length()) > sublen) {
+	 LinearExtent chunkext(0, sublen);
 
 //      cerr << "chunkext == " << chunkext
 //	   << " && subext_ == " << subext_ << "\n";
-      subext_ = chunkext.SubExtent(subext_);
+	 subext_.SubExtent_eq(chunkext);
 //      cerr << "subext_ == " << subext_ << "\n";
+      }
    }
 }
