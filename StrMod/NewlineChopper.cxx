6673fd0fdd5121a103e76456c63777a9b55522c7
Eric Hopper <hopper@omnifarious.org>
1999-07-24 21:42:51 +0000

    Updated to new coding standards.

--- StrMod/NewlineChopper.cxx
+++ StrMod/NewlineChopper.cxx
@@ -1,6 +1,7 @@
 /* $Header$ */
 
-// $Log$
+// For a log, see ChangeLog
+//
 // Revision 1.1  1996/09/02 23:18:40  hopper
 // Added NewlineChopper class so users would have a simple class that
 // would break up and recombine streams using '\n' as a separator.
--- StrMod/StrMod/NewlineChopper.h
+++ StrMod/StrMod/NewlineChopper.h
@@ -6,7 +6,8 @@
 
 /* $Header$ */
 
-// $Log$
+// For a log, see ../ChangeLog
+//
 // Revision 1.1  1996/09/02 23:18:48  hopper
 // Added NewlineChopper class so users would have a simple class that
 // would break up and recombine streams using '\n' as a separator.
