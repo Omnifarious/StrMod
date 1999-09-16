4011a93b8914545382646d54b4e4f4adaf1b9f18
Eric Hopper <hopper@omnifarious.org>
1999-09-16 00:16:27 +0000

    Added a class, derived from BufferChunk, to represent a bag of bytes that's always allocated from the heap.

---
+++ StrMod/DynamicBuffer.cxx
@@ -0,0 +1,62 @@
+/* $Header$ */
+
+// For a log, see ./ChangeLog
+
+#ifdef __GNUG__
+#  pragma implementation "DynamicBuffer.h"
+#endif
+
+#include "StrMod/DynamicBuffer.h"
+#include <cstdlib>
+#include <new>
+#include <cassert>
+
+const STR_ClassIdent DynamicBuffer::identifier(39UL);
+
+DynamicBuffer::~DynamicBuffer()
+{
+   if (buf_)
+   {
+      assert(buflen_ > 0);
+      free(buf_);
+      buf_ = 0;
+      buflen_ = 0;
+   }
+}
+
+void DynamicBuffer::resize(unsigned int newsize) throw(bad_alloc)
+{
+   #ifndef NDEBUG
+   if (buf_ != 0)
+   {
+      assert((buf_ != 0) && (buflen_ > 0));
+   }
+   else
+   {
+      assert((buf_ == 0) && (buflen_ == 0));
+   }
+   #endif
+
+   if (newsize != buflen_)
+   {
+      if (newsize == 0)
+      {
+	 free(buf_);
+	 buf_ = 0;
+	 buflen_ = newsize;
+      }
+      else
+      {
+	 void *newbuf = realloc(buf_, newsize);
+	 if ((newbuf == NULL) && (newsize != 0))
+	 {
+	    throw bad_alloc();
+	 }
+	 else
+	 {
+	    buf_ = static_cast<U1Byte *>(newbuf);
+	    buflen_ = newsize;
+	 }
+      }
+   }
+}
---
+++ StrMod/StrMod/DynamicBuffer.h
@@ -0,0 +1,41 @@
+#ifndef _STR_DynamicBuffer_H_  // -*-c++-*-
+
+#ifdef __GNUG__
+#  pragma interface
+#endif
+
+/* $Header$ */
+
+// For a log, see ../ChangeLog
+
+#include <StrMod/BufferChunk.h>
+
+#define _STR_DynamicBuffer_H_
+
+//: A completely dyanmically allocated bag of bytes.
+class DynamicBuffer : public BufferChunk {
+ public:
+   static const STR_ClassIdent identifier;
+
+   DynamicBuffer()                             { }
+   virtual ~DynamicBuffer();
+
+   inline virtual int AreYouA(const ClassIdent &cid) const;
+
+   virtual void resize(unsigned int newsize) throw(bad_alloc);
+
+ protected:
+   virtual const ClassIdent *i_GetIdent() const         { return(&identifier); }
+
+   virtual void i_DropUnused(const LinearExtent &usedextent,
+			     KeepDir keepdir)      { }
+};
+
+//-----------------------------inline functions--------------------------------
+
+inline int DynamicBuffer::AreYouA(const ClassIdent &cid) const
+{
+   return((identifier == cid) || BufferChunk::AreYouA(cid));
+}
+
+#endif
