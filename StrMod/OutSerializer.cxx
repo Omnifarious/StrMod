4dca3148a74b8c97c073c09c0963d24672730262
Eric Hopper <hopper@omnifarious.org>
1999-09-16 00:37:52 +0000

    Changed to conform to new coding standards.
    Changed to use BufferChunk instead of old DataBlockStrChunk.
    
    Changed to allow the internal state to be stored externally so the
    instance could be destructed, and then reconstructed in its old state.

--- StrMod/OutSerializer.cxx
+++ StrMod/OutSerializer.cxx
@@ -1,6 +1,7 @@
 /* $Header$ */
 
-// $Log$
+// For a log, see ./ChangeLog
+//
 // Revision 1.2  1999/01/12 04:13:40  hopper
 // All kinds of changes to make the code more portable and compile
 // properly with the DEC compiler.
@@ -13,75 +14,132 @@
 #  pragma implementation "OutSerializer.h"
 #endif
 
-#include <StrMod/OutSerializer.h>
-#include <StrMod/DBStrChunk.h>
+#include "StrMod/OutSerializer.h"
+#include "StrMod/BufferChunkFactory.h"
+#include "StrMod/DynamicBuffer.h"
 
-OutSerializer::OutSerializer(size_t suggested_size) : cur_pos_(0)
+inline OutSerializer::State::State(BufferChunk::Factory *fact)
+     : fact_(fact), cur_chunk_(0), buf_(0), chnklen_(0), cur_pos_(0)
 {
-   cur_chunk_ = new DataBlockStrChunk(suggested_size);
-   chnklen_ = cur_chunk_->Length();
-   buf_ = cur_chunk_->GetCharP();
 }
 
-OutSerializer::OutSerializer() : cur_pos_(0)
+inline OutSerializer::State::State(const State &b)
+     : fact_(b.fact_), cur_chunk_(b.cur_chunk_),
+       buf_(b.buf_), chnklen_(b.chnklen_), cur_pos_(b.cur_pos_)
 {
-   cur_chunk_ = new DataBlockStrChunk(256);
-   chnklen_ = cur_chunk_->Length();
-   buf_ = cur_chunk_->GetCharP();
+}
+
+inline const OutSerializer::State &
+OutSerializer::State::operator =(const State &b)
+{
+   cur_chunk_ = b.cur_chunk_;
+   buf_ = b.buf_;
+   chnklen_ = b.chnklen_;
+   cur_pos_ = b.cur_pos_;
+   return(*this);
+}
+
+OutSerializer::OutSerializer(size_t suggested_size)
+	  : state_(0), external_state_(0)
+{
+   state_.cur_chunk_ = new DynamicBuffer;
+   state_.cur_chunk_->resize(suggested_size);
+   state_.chnklen_ = state_.cur_chunk_->Length();
+   state_.buf_ = state_.cur_chunk_->getCharP();
+}
+
+OutSerializer::OutSerializer(State &savedstate)
+     : state_(savedstate), external_state_(&savedstate)
+{
+   if (state_.cur_chunk_ == 0)
+   {
+      assert(state_.fact_ != 0);
+      state_.cur_chunk_ = state_.fact_->makeChunk();
+      state_.chnklen_ = state_.cur_chunk_->Length();
+      state_.cur_pos_ = 0;
+   }
+   else
+   {
+      assert(state_.chnklen_ == state_.cur_chunk_->Length());
+      assert(state_.cur_pos_ <= state_.chnklen_);
+   }
+}
+
+OutSerializer::OutSerializer() : state_(0), external_state_(0)
+{
+   state_.cur_chunk_ = new DynamicBuffer;
+   state_.cur_chunk_->resize(256);
+   state_.cur_pos_ = 0;
+   state_.chnklen_ = state_.cur_chunk_->Length();
+   state_.buf_ = state_.cur_chunk_->getCharP();
 }
 
 OutSerializer::~OutSerializer()
 {
-   if (cur_chunk_) {
-      delete cur_chunk_;
+   if (external_state_)
+   {
+      *external_state_ = state_;
+   }
+   else if (state_.cur_chunk_)
+   {
+      delete state_.cur_chunk_;
+      state_.cur_chunk_ = 0;
    }
 }
 
-static void AddCharStr(OutSerializer &out, const char *data, size_t len)
+static void SAddCharStr(OutSerializer &out, const char *data, size_t len)
 {
-   if (len >= 65535) {
+   if (len >= 65535)
+   {
       len = 65534;
    }
 
    U2Byte netlen = len;
 
-   out.AddU2Byte(netlen + 1);  // Add 1 for trailing '\0';
-   out.AddRaw(data, netlen);
-   out.AddU1Byte('\0');  // Add trailing '\0';
+   out.addU2Byte(netlen + 1);  // Add 1 for trailing '\0';
+   out.addRaw(data, netlen);
+   out.addU1Byte('\0');  // Add trailing '\0';
 }
 
-void OutSerializer::AddString(const string &str)
+void OutSerializer::addString(const string &str)
 {
    size_t len = str.length();
 
-   AddCharStr(*this, str.c_str(), len);
+   SAddCharStr(*this, str.c_str(), len);
 }
 
-void OutSerializer::AddString(const char *str)
+void OutSerializer::addString(const char *str)
 {
-   AddCharStr(*this, str, strlen(str));
+   SAddCharStr(*this, str, strlen(str));
 }
 
-DataBlockStrChunk *OutSerializer::TakeChunk()
+BufferChunk *OutSerializer::takeChunk()
 {
-   assert(cur_chunk_ != 0);
+   assert(state_.cur_chunk_ != 0);
 
-   DataBlockStrChunk *temp = cur_chunk_;
+   BufferChunk *temp = state_.cur_chunk_;
 
-   cur_chunk_ = 0;
-   buf_ = 0;
-   chnklen_ = 0;
-   temp->Resize(cur_pos_);
-   cur_pos_ = 0;
+   state_.cur_chunk_ = 0;
+   state_.buf_ = 0;
+   state_.chnklen_ = 0;
+   temp->resize(state_.cur_pos_);
+   state_.cur_pos_ = 0;
    return(temp);
 }
 
-void OutSerializer::ResizeChunk(size_t newsize)
+void OutSerializer::resizeChunk(size_t newsize)
 {
-   assert(cur_chunk_ != 0);
+   assert(state_.cur_chunk_ != 0);
 
-   cur_chunk_->Resize(newsize + newsize / 16);
-   buf_ = cur_chunk_->GetCharP();
-   chnklen_ = cur_chunk_->Length();
-   assert(chnklen_ >= newsize);
+   if ((newsize - state_.chnklen_) < 256)
+   {
+      state_.cur_chunk_->resize(newsize + newsize / 16);
+   }
+   else
+   {
+      state_.cur_chunk_->resize(newsize + newsize / 256);
+   }
+   state_.buf_ = state_.cur_chunk_->getCharP();
+   state_.chnklen_ = state_.cur_chunk_->Length();
+   assert(state_.chnklen_ >= newsize);
 }
--- StrMod/StrMod/OutSerializer.h
+++ StrMod/StrMod/OutSerializer.h
@@ -6,7 +6,8 @@
 
 /* $Header$ */
 
-// $Log$
+// For a log, see ../ChangeLog
+//
 // Revision 1.2  1998/06/02 00:30:53  hopper
 // Fixed a small bug that caused adding a 2 bytes value to not work
 // correctly.
@@ -15,6 +16,7 @@
 // Added OutSerializer class for simple serialization of basic data types.
 //
 
+#include <StrMod/BufferChunk.h>
 #include <LCore/GenTypes.h>
 #include <string.h>
 #include <string>
@@ -22,157 +24,184 @@
 
 #define _STR_OutSerializer_H_
 
-class DataBlockStrChunk;
-
 class OutSerializer {
  public:
+   struct State;
+
    OutSerializer(size_t suggested_size);
+   //: Use this as the external state to save into on destruction.  Only use
+   //: this if you've read the directions.  :-)
+   // <p>This class can be much more efficient if it's always instantiated
+   // locally.  Sometimes though, the state will need to be saved across
+   // function calls.  That's where this constructor comes in.</p>
+   // <p>If the <code>takeChunk</code> function is called, a flag is set in
+   // the external state (that is also set when the external state is
+   // initially constructed) that tells this OutSerializer constructor that it
+   // needs to reset the state.</p>
+   OutSerializer(State &savedstate);
    OutSerializer();
    virtual ~OutSerializer();
 
-   inline void AddS1Byte(S1Byte num);
-   inline void AddU1Byte(U1Byte num);
+   inline void addS1Byte(S1Byte num);
+   inline void addU1Byte(U1Byte num);
 
-   inline void AddS2Byte(S2Byte num);
-   inline void AddU2Byte(U2Byte num);
+   inline void addS2Byte(S2Byte num);
+   inline void addU2Byte(U2Byte num);
 
-   inline void AddS4Byte(S4Byte num);
-   inline void AddU4Byte(U4Byte num);
+   inline void addS4Byte(S4Byte num);
+   inline void addU4Byte(U4Byte num);
 
-   void AddBool(bool val)                           { AddU1Byte(U1Byte(val)); }
+   void addBool(bool val)                            { addU1Byte(U1Byte(val)); }
 
-   void AddString(const string &str);
-   void AddString(const char *str);  // C style string.
+   void addString(const string &str);
+   void addString(const char *str);  // C style string.
 
-   inline void AddRaw(const void *data, size_t len);
+   inline void addRaw(const void *data, size_t len);
 
-   inline void SetMinSuggestedSize(size_t size);
+   inline void setMinSuggestedSize(size_t size);
 
-   // It is an error to call this function more than once.  The chunk
-   // you get has exactly the number of bytes you put in.
-   DataBlockStrChunk *TakeChunk();
+   //: It is an error to call this function more than once.  The chunk you get
+   //: has exactly the number of bytes you put in.
+   BufferChunk *takeChunk();
+
+   //: Holds the state of the OutSerializer so it can be saved and restored.
+   // <p>All inline functions here are defined in the .cxx fileI have heard th
+   class State {
+    public:
+      friend class OutSerializer;
+      //: Assumes ownership of fact.
+      inline State(BufferChunk::Factory *fact);
+
+    private:
+      BufferChunk::Factory * const fact_;
+      BufferChunk *cur_chunk_;
+      U1Byte *buf_;
+      size_t chnklen_;
+      size_t cur_pos_;
+
+      inline State(const State &b);
+      inline const State &operator =(const State &b) ;
+   };
 
  private:
-   DataBlockStrChunk *cur_chunk_;
-   U1Byte *buf_;
-   size_t chnklen_;
-   size_t cur_pos_;
+   struct State state_;
+   struct State * const external_state_;
 
-   void ResizeChunk(size_t newsize);
+   void resizeChunk(size_t newsize);
 };
 
 //-----------------------------inline functions--------------------------------
 
-inline void OutSerializer::SetMinSuggestedSize(size_t size)
+inline void OutSerializer::setMinSuggestedSize(size_t size)
 {
-   if (size > chnklen_) {
-      ResizeChunk(size);
+   if (size > state_.chnklen_) {
+      resizeChunk(size);
    }
 }
 
-inline void OutSerializer::AddS1Byte(S1Byte num)
+inline void OutSerializer::addS1Byte(S1Byte num)
 {
-   SetMinSuggestedSize(cur_pos_ + 1);
+   setMinSuggestedSize(state_.cur_pos_ + 1);
 
-   hton(num, buf_ + cur_pos_);
-   cur_pos_++;
+   hton(num, state_.buf_ + state_.cur_pos_);
+   state_.cur_pos_++;
 }
 
-inline void OutSerializer::AddU1Byte(U1Byte num)
+inline void OutSerializer::addU1Byte(U1Byte num)
 {
-   SetMinSuggestedSize(cur_pos_ + 1);
+   setMinSuggestedSize(state_.cur_pos_ + 1);
 
-   hton(num, buf_ + cur_pos_);
-   cur_pos_++;
+   hton(num, state_.buf_ + state_.cur_pos_);
+   state_.cur_pos_++;
 }
 
-inline void OutSerializer::AddS2Byte(S2Byte num)
+inline void OutSerializer::addS2Byte(S2Byte num)
 {
-   SetMinSuggestedSize(cur_pos_ + 2);
+   setMinSuggestedSize(state_.cur_pos_ + 2);
 
-   hton(num, buf_ + cur_pos_);
-   cur_pos_ += 2;
+   hton(num, state_.buf_ + state_.cur_pos_);
+   state_.cur_pos_ += 2;
 }
 
-inline void OutSerializer::AddU2Byte(U2Byte num)
+inline void OutSerializer::addU2Byte(U2Byte num)
 {
-   SetMinSuggestedSize(cur_pos_ + 2);
+   setMinSuggestedSize(state_.cur_pos_ + 2);
 
-   hton(num, buf_ + cur_pos_);
-   cur_pos_ += 2;
+   hton(num, state_.buf_ + state_.cur_pos_);
+   state_.cur_pos_ += 2;
 }
 
-inline void OutSerializer::AddS4Byte(S4Byte num)
+inline void OutSerializer::addS4Byte(S4Byte num)
 {
-   SetMinSuggestedSize(cur_pos_ + 4);
+   setMinSuggestedSize(state_.cur_pos_ + 4);
 
-   hton(num, buf_ + cur_pos_);
-   cur_pos_ += 4;
+   hton(num, state_.buf_ + state_.cur_pos_);
+   state_.cur_pos_ += 4;
 }
 
-inline void OutSerializer::AddU4Byte(U4Byte num)
+inline void OutSerializer::addU4Byte(U4Byte num)
 {
-   SetMinSuggestedSize(cur_pos_ + 4);
+   setMinSuggestedSize(state_.cur_pos_ + 4);
 
-   hton(num, buf_ + cur_pos_);
-   cur_pos_ += 4;
+   hton(num, state_.buf_ + state_.cur_pos_);
+   state_.cur_pos_ += 4;
 }
 
-inline void OutSerializer::AddRaw(const void *data, size_t len)
+inline void OutSerializer::addRaw(const void *data, size_t len)
 {
-   SetMinSuggestedSize(cur_pos_ + len);
+   setMinSuggestedSize(state_.cur_pos_ + len);
 
-   memcpy(buf_ + cur_pos_, data, len);
-   cur_pos_ += len;
+   memcpy(state_.buf_ + state_.cur_pos_, data, len);
+   state_.cur_pos_ += len;
 }
 
 //---
 
 inline OutSerializer &operator <<(OutSerializer &os, S1Byte num)
 {
-   os.AddS1Byte(num);
+   os.addS1Byte(num);
    return(os);
 }
 
 inline OutSerializer &operator <<(OutSerializer &os, U1Byte num)
 {
-   os.AddU1Byte(num);
+   os.addU1Byte(num);
    return(os);
 }
 
 inline OutSerializer &operator <<(OutSerializer &os, S2Byte num)
 {
-   os.AddS2Byte(num);
+   os.addS2Byte(num);
    return(os);
 }
 
 inline OutSerializer &operator <<(OutSerializer &os, U2Byte num)
 {
-   os.AddU2Byte(num);
+   os.addU2Byte(num);
    return(os);
 }
 
 inline OutSerializer &operator <<(OutSerializer &os, S4Byte num)
 {
-   os.AddS4Byte(num);
+   os.addS4Byte(num);
    return(os);
 }
 
 inline OutSerializer &operator <<(OutSerializer &os, U4Byte num)
 {
-   os.AddU4Byte(num);
+   os.addU4Byte(num);
    return(os);
 }
 
 inline OutSerializer &operator <<(OutSerializer &os, const string &str)
 {
-   os.AddString(str);
+   os.addString(str);
    return(os);
 }
 
 inline OutSerializer &operator <<(OutSerializer &os, const char *str)
 {
-   os.AddString(str);
+   os.addString(str);
    return(os);
 }
 
