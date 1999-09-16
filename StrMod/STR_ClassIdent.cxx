e062e8a6f22c8a80a30cf9f5c4e9ab56e4ddea15
Eric Hopper <hopper@omnifarious.org>
1999-09-16 01:03:58 +0000

    Added all of the new classes.

--- StrMod/STR_ClassIdent.cxx
+++ StrMod/STR_ClassIdent.cxx
@@ -34,7 +34,7 @@ const STR_ClassIdent STR_ClassIdent::identifier(0UL);
   21UL	SimpleMultiplexer
   22UL	SimpleMultiplexer::MultiPlug
   23UL	SimpleMultiplexer::SinglePlug
-  24UL  SimplePlug
+  24UL  BufferChunk
   25UL	StreamProcessor
   26UL	CharChopper
   27UL	NewlineChopper
@@ -43,4 +43,12 @@ const STR_ClassIdent STR_ClassIdent::identifier(0UL);
   30UL	ProcessorModule::PMPlug
   31UL	EOFStrChunk
   32UL	SimpleMultiplexer::ScanEvent
+  33UL	TelnetParser
+  34UL	TelnetParser::TelnetData
+  35UL	TelnetParser::SingleChar
+  36UL	TelnetParser::Suboption
+  37UL	TelnetParser::OptionNegotiation
+  38UL	PreAllocBufferBase
+  39UL	DynamicBuffer
+  40UL	BufferChunk::Factory
 */
