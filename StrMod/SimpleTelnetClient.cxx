b645a698ae36003f469d7878f53f3fd4bb95b617
Eric Hopper <hopper@omnifarious.org>
1999-10-15 00:45:48 +0000

    Added the SimpleTelnetClient class. This is designed for simple, character at a time, telnet sessions.

---
+++ StrMod/SimpleTelnetClient.cxx
@@ -0,0 +1,219 @@
+/* $Header$ */
+
+// For a log, see ChangeLog
+
+#ifdef __GNUG__
+#  pragma implementation "SimpleTelnetClient.h"
+#endif
+
+#include "StrMod/SimpleTelnetClient.h"
+#include "StrMod/TelnetParserData.h"
+
+const STR_ClassIdent SimpleTelnetClient::identifier(43UL);
+const STR_ClassIdent SimpleTelnetClient::UPlug::identifier(44UL);
+const STR_ClassIdent SimpleTelnetClient::SPlug::identifier(45UL);
+
+SimpleTelnetClient::SimpleTelnetClient()
+     : sent_do_supga_(false), sent_do_echo_(false),
+       uplugcreated_(false), userplug_(*this),
+       splugcreated_(false), serverplug_(*this)
+{
+   reset();
+}
+
+SimpleTelnetClient::~SimpleTelnetClient()
+{
+}
+
+bool SimpleTelnetClient::deletePlug(Plug *plug)
+{
+   if ((plug == &userplug_) && uplugcreated_)
+   {
+      uplugcreated_ = false;
+      userplug_.unPlug();
+      return(true);
+   }
+   else if ((plug == &serverplug_) && splugcreated_)
+   {
+      splugcreated_ = false;
+      serverplug_.unPlug();
+      return(true);
+   }
+   return(false);
+}
+
+void SimpleTelnetClient::reset()
+{
+   sent_do_supga_ = false;
+   sent_do_echo_ = false;
+   if (doProtocol())
+   {
+      updatePlugFlags();
+   }
+}
+
+StreamModule::Plug *SimpleTelnetClient::i_MakePlug(int side)
+{
+   assert(((side == ToServer) && !splugcreated_) ||
+	  ((side == ToUser) && !uplugcreated_));
+
+   if (side == ToServer)
+   {
+      splugcreated_ = true;
+      return(&serverplug_);
+   }
+   else
+   {
+      uplugcreated_ = true;
+      return(&userplug_);
+   }
+}
+
+bool SimpleTelnetClient::doProtocol()
+{
+   const U1Byte echoopt = 1;
+   const U1Byte supgaopt = 3;
+
+   if ((!sent_do_supga_ || !sent_do_echo_) && !toserver_)
+   {
+      U1Byte sendopt = echoopt;
+      if (!sent_do_supga_)
+      {
+	 sendopt = supgaopt;
+	 sent_do_supga_ = true;
+      }
+      else
+      {
+	 sent_do_echo_ = true;
+      }
+
+      toserver_ = new TelnetParser::OptionNegotiation(TelnetParser::OptionNegotiation::DO, sendopt);
+      return(true);
+   }
+   return(false);
+}
+
+void SimpleTelnetClient::updatePlugFlags()
+{
+   setReadableFlagFor(&serverplug_,
+		      (toserver_ || (uplugcreated_
+				     && userplug_.canReadOther())));
+   setWriteableFlagFor(&userplug_,
+		       (!toserver_ && splugcreated_
+			&& serverplug_.canWriteOther()));
+
+   setReadableFlagFor(&userplug_, touser_);
+   setWriteableFlagFor(&serverplug_, (!touser_ && !toserver_));
+}
+
+const StrChunkPtr SimpleTelnetClient::userRead()
+{
+   StrChunkPtr tmp = touser_;
+   touser_.ReleasePtr();
+   updatePlugFlags();
+   return(tmp);
+}
+
+void SimpleTelnetClient::serverWrite(const StrChunkPtr &ptr)
+{
+   assert(!toserver_);
+   if (ptr->AreYouA(TelnetParser::TelnetData::identifier))
+   {
+      if (ptr->AreYouA(TelnetParser::OptionNegotiation::identifier))
+      {
+	 StrChunkPtrT<TelnetParser::OptionNegotiation> optneg =
+	    static_cast<TelnetParser::OptionNegotiation *>(ptr.GetPtr());
+
+	 if (optneg->getRequest() == TelnetParser::OptionNegotiation::WILL)
+	 {
+	    if ((optneg->getType() != 1) && (optneg->getType() != 3))
+	    {
+	       toserver_ = new TelnetParser::OptionNegotiation(
+		  TelnetParser::OptionNegotiation::DONT, optneg->getType());
+	    }
+	 }
+	 else if (optneg->getRequest() == TelnetParser::OptionNegotiation::DO)
+	 {
+	    if (optneg->getType() == 3)
+	    {
+	       toserver_ = new TelnetParser::OptionNegotiation(
+		  TelnetParser::OptionNegotiation::WILL, 3);
+	    }
+	    else
+	    {
+	       toserver_ = new TelnetParser::OptionNegotiation(
+		  TelnetParser::OptionNegotiation::WONT, optneg->getType());
+	    }
+	 }
+	 else if (optneg->getRequest() == TelnetParser::OptionNegotiation::DONT)
+	 {
+	    toserver_ = new TelnetParser::OptionNegotiation(
+	       TelnetParser::OptionNegotiation::WONT, optneg->getType());
+	 }
+	 // WONTs are always ignored.  The rule about no automated retries of
+	 // failed negotiations prevents us from resending a DO if the server
+	 // refuses SUPRESS GOAHREAD or ECHO.  We'll just pretend it didn't
+	 // because it doesn't really matter in terms of how we process
+	 // things.
+      }
+      // Anything other than option negotiation is also ignored.
+   }
+   else
+   {
+      touser_ = ptr;
+   }
+   updatePlugFlags();
+}
+
+const StrChunkPtr SimpleTelnetClient::SPlug::i_Read()
+{
+   SimpleTelnetClient &parent = getParent();
+   if (parent.toserver_)
+   {
+      StrChunkPtr tmp = parent.toserver_;
+      parent.toserver_.ReleasePtr();
+      if (!parent.doProtocol())  // if doProtocol didn't put more in toserver_
+      {
+	 parent.updatePlugFlags();
+      }
+      return(tmp);
+   }
+   else
+   {
+      assert(parent.uplugcreated_);
+      UPlug &sibling = parent.userplug_;
+
+      assert(sibling.pluggedInto());
+      Plug &readfrom = *(sibling.pluggedInto());
+
+      assert(readfrom.isReadable());
+
+      setIsReading(readfrom, true);
+      StrChunkPtr tmp = sibling.readOther();
+      setIsReading(readfrom, false);
+      return(tmp);
+   }
+}
+
+void SimpleTelnetClient::UPlug::i_Write(const StrChunkPtr &ptr)
+{
+   SimpleTelnetClient &parent = getParent();
+   assert(!parent.toserver_ && parent.splugcreated_
+	  && parent.serverplug_.canWriteOther());
+   assert(isWriteable());
+
+   assert(parent.serverplug_.pluggedInto());
+   Plug &writeto = *(parent.serverplug_.pluggedInto());
+
+   if (getFlagsFrom(writeto).iswriting_)
+   {
+      parent.toserver_ = ptr;
+      setWriteable(false);
+   }
+   else
+   {
+      setIsWriting(writeto, true);
+      parent.serverplug_.writeOther(ptr);
+      setIsWriting(writeto, false);
+   }
+}
---
+++ StrMod/StrMod/SimpleTelnetClient.h
@@ -0,0 +1,208 @@
+#ifndef _STR_SimpleTelnet_H_  // -*-c++-*-
+
+#ifdef __GNUG__
+#  pragma interface
+#endif
+
+/* $Header$ */
+
+// For a log, see ../ChangeLog
+
+#include <StrMod/StreamModule.h>
+#include <StrMod/STR_ClassIdent.h>
+
+#define _STR_SimpleTelnet_H_
+
+//: Only will handle the case when the server will do supress go ahead, and
+//: echo.
+// <p>This is a very simple telnet protocol handler.  It won't support ANY
+// options besides the server doing supress go ahead, and echo, and it
+// requires the server to support and enable those options.  This means that
+// it will only do 'character at a time' mode.</p>
+class SimpleTelnetClient : public StreamModule {
+   class UPlug;
+   class SPlug;
+   friend class UPlug;
+   friend class SPlug;
+ public:
+   enum Sides { ToServer, ToUser };
+   static const STR_ClassIdent identifier;
+
+   SimpleTelnetClient();
+   ~SimpleTelnetClient();
+
+   //: See base class.
+   inline virtual int AreYouA(const ClassIdent &cid) const;
+
+   //: See base class.
+   inline virtual bool canCreate(int side) const;
+   //: See base class.
+   inline virtual bool ownsPlug(const Plug *plug) const;
+   //: See base class.
+   virtual bool deletePlug(Plug *plug);
+
+   //: Resets the states of the sent_do_supga_ and sent_do_echo_ flags.
+   // <p>Resetting these flags has the effect of causing the client to attempt
+   // to renegotiate these options</p>
+   void reset();
+
+ protected:
+   virtual const ClassIdent *i_GetIdent() const         { return(&identifier); }
+
+   virtual Plug *i_MakePlug(int side);
+
+   //: Handles sending telnet protocol messages for initial negotiation
+   bool doProtocol();
+   //: Checks the state of things and updates all the readable and writeable
+   //: flags.
+   void updatePlugFlags();
+
+   //: Called when a read is done from the user plug.
+   const StrChunkPtr userRead();
+
+   //: Called when a write is done on the server plug.
+   // <p>This handles properly replying to the messages the server sends.</p>
+   void serverWrite(const StrChunkPtr &ptr);
+
+ private:
+   class UPlug : public Plug {
+    public:
+      friend class SimpleTelnetClient;
+		friend class SPlug;
+      
+      static const STR_ClassIdent identifier;
+
+      UPlug(SimpleTelnetClient &parent) : Plug(parent)  { }
+      ~UPlug()                                          { }
+
+      inline virtual int AreYouA(const ClassIdent &cid) const;
+
+      inline SimpleTelnetClient &getParent() const;
+      virtual int side() const                          { return(ToUser); }
+
+    protected:
+      virtual const ClassIdent *i_GetIdent() const      { return(&identifier); }
+
+      virtual bool needsNotifyReadable() const          { return(true); }
+      inline virtual void otherIsReadable();
+
+      inline virtual const StrChunkPtr i_Read();
+      inline virtual void i_Write(const StrChunkPtr &chnk);
+
+      inline bool canReadOther() const;
+   };
+
+   class SPlug : public Plug {
+    public:
+      friend class SimpleTelnetClient;
+		friend class UPlug;
+      
+      static const STR_ClassIdent identifier;
+
+      SPlug(SimpleTelnetClient &parent) : Plug(parent)  { }
+      ~SPlug()                                          { }
+
+      inline virtual int AreYouA(const ClassIdent &cid) const;
+
+      inline SimpleTelnetClient &getParent() const;
+      virtual int side() const                          { return(ToServer); }
+
+    protected:
+      virtual const ClassIdent *i_GetIdent() const      { return(&identifier); }
+
+      virtual bool needsNotifyWriteable() const         { return(true); }
+      inline virtual void otherIsWriteable();
+
+      inline virtual const StrChunkPtr i_Read();
+      inline virtual void i_Write(const StrChunkPtr &chnk);
+
+      inline bool canWriteOther() const;
+   };
+
+ private:
+   bool sent_do_supga_;
+   bool sent_do_echo_;
+   StrChunkPtr toserver_;
+   StrChunkPtr touser_;
+   bool uplugcreated_;
+   UPlug userplug_;
+   bool splugcreated_;
+   SPlug serverplug_;
+};
+
+//------------------------------inline function--------------------------------
+
+inline int SimpleTelnetClient::AreYouA(const ClassIdent &cid) const
+{
+   return((cid == identifier) || StreamModule::AreYouA(cid));
+}
+
+inline bool SimpleTelnetClient::canCreate(int side) const
+{
+   return(((side == ToServer) && !splugcreated_) ||
+	  ((side == ToUser)   && !uplugcreated_));
+}
+
+inline  bool SimpleTelnetClient::ownsPlug(const Plug *plug) const
+{
+   return((splugcreated_ && (plug == &serverplug_)) ||
+	  (uplugcreated_ && (plug == &userplug_)));
+}
+
+//--
+
+inline int SimpleTelnetClient::UPlug::AreYouA(const ClassIdent &cid) const
+{
+   return((identifier == cid) || Plug::AreYouA(cid));
+}
+
+inline SimpleTelnetClient &SimpleTelnetClient::UPlug::getParent() const
+{
+   return(static_cast<SimpleTelnetClient &>(Plug::getParent()));
+}
+
+inline void SimpleTelnetClient::UPlug::otherIsReadable()
+{
+   getParent().updatePlugFlags();
+}
+
+inline const StrChunkPtr SimpleTelnetClient::UPlug::i_Read()
+{
+   return(getParent().userRead());
+}
+
+inline bool SimpleTelnetClient::UPlug::canReadOther() const
+{
+   Plug *other = pluggedInto();
+   return((other == NULL) ? false : getFlagsFrom(*other).canread_);
+}
+
+//--
+
+inline int SimpleTelnetClient::SPlug::AreYouA(const ClassIdent &cid) const
+{
+   return((identifier == cid) || Plug::AreYouA(cid));
+}
+
+inline SimpleTelnetClient &SimpleTelnetClient::SPlug::getParent() const
+{
+   return(static_cast<SimpleTelnetClient &>(Plug::getParent()));
+}
+
+inline void SimpleTelnetClient::SPlug::otherIsWriteable()
+{
+   getParent().updatePlugFlags();
+}
+
+inline void SimpleTelnetClient::SPlug::i_Write(const StrChunkPtr &chnk)
+{
+   getParent().serverWrite(chnk);
+}
+
+inline bool SimpleTelnetClient::SPlug::canWriteOther() const
+{
+   Plug *other = pluggedInto();
+   return((other == NULL) ? false : getFlagsFrom(*other).canwrite_);
+}
+
+#endif
