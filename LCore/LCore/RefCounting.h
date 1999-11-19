22dfb597c80209ba9aba049e31799e1edd9710c3
Eric Hopper <hopper@omnifarious.org>
1999-11-19 05:20:57 +0000

    Added copyright notices everywhere!

--- LCore/LCore/RefCounting.h
+++ LCore/LCore/RefCounting.h
@@ -1,8 +1,27 @@
 #ifndef _EH_0_RefCounting_H_
 
+/*
+ * Copyright (C) 1991-9 Eric M. Hopper <hopper@omnifarious.mn.org>
+ * 
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU Lesser General Public License as published
+ *     by the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 /* $Header$ */
 
-// $Log$
+// For a log, see ../ChangeLog
+//
 // Revision 1.2  1997/05/12 16:27:53  hopper
 // Fixed ReferenceCounting::NumReferences to be a const method, as befits
 // an accessor.
