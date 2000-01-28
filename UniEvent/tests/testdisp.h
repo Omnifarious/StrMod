e0205c9bdeab686024549f1f7bedfe487a14d58a
Eric Hopper <hopper@omnifarious.org>
2000-01-28 02:31:29 +0000

    Removed a reference to my badly thought out <bool.h> header file.

--- UniEvent/tests/testdisp.h
+++ UniEvent/tests/testdisp.h
@@ -26,12 +26,10 @@
 
 // See ../ChangeLog file for log
 
-#include <bool.h>
-
 #define _UNEVT_testdisp_H_
 
 class UNIDispatcher;
 
-bool_val TestDispatcher(UNIDispatcher &disp, long seed);
+bool TestDispatcher(UNIDispatcher &disp, long seed);
 
 #endif
