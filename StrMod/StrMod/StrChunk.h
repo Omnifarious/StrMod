2d51ed8ab5828375d20f1214ff83fc11678c3529
Eric Hopper <hopper@omnifarious.org>
2000-12-07 03:42:36 +0000

    Removed all the old GroupVector based member functions, DropUnused, and i_DropUnused. All of the functionality in the GroupVector and related classes can be had using the ChunkIterator or ChunkVisitor classes.  The DropUnused and i_DropUnused functionality wasn't used often, and will be revisited later. Also, completely worked over documentation for Doxygen.

--- StrMod/StrMod/StrChunk.h
+++ StrMod/StrMod/StrChunk.h
@@ -44,80 +44,72 @@
 #define _STR_StrChunk_H_
 
 class LinearExtent;
-class GroupVector;
 
-//------------------------------class StrChunk---------------------------------
-
-//: A reference counted chunk of data.
-// Use the StrChunkPtr class if you want the reference count to be maintained
-// automatically.
+/** \class StrChunk StrChunk.h StrMod/StrChunk.h
+ * An interface to a reference counted chunk of data.
+ *
+ * The chunk of data is actually held by the derived classes.  Some chunks of
+ * data even consist of other StrChunks.  This is an instance of the <a
+ * href="http://exciton.cs.oberlin.edu/javaresources/DesignPatterns/composite.htm">Composite</a>
+ * design pattern.
+ *
+ * StrChunks are supposed to be immutable after they're created.  Often
+ * derived classes will have methods for updating the data held within, but
+ * StrChunks don't, and they're what the outside world sees.
+ *
+ * StrChunks are reference counted.  Use the StrChunkPtr class if you want a
+ * nice smart pointer that will automatically maintain the reference count and
+ * delete the chunk when necessary.
+ *
+ * StrChunks are reference counted because the containment hierarchy for them
+ * is a DAG.  Just because the StrChunk is found by traversing a tree rooted
+ * at a particular StrChunk doesn't mean that same StrChunk can't be found in
+ * a different tree.  It might even be found twice in the same tree.  This
+ * complexity requires reference counts for tractable resource handling.
+ */
 class StrChunk : public Object, public ReferenceCounting {
    friend class ChunkVisitor;
  public:
-   //: Describes how 0 length chunks should be handled when chunks are dropped.
-   // <dl><dt>KeepLeft</dt> <dd>Drop all trailing 0 length chunks and keep all
-   // leading 0 length chunks.</dd>
-   // <dt>KeepRight</dt> <dd>Dropp all leading 0 length chunks and keep all
-   // trailing 0 length chunks.</dd>
-   // <dt>KeepNone</dt> <dd>Drop both leading and trailing 0 length
-   // chunks.</dd></dl>
-   enum KeepDir { KeepLeft, KeepRight, KeepNone };
    class __iterator;
    friend class __iterator;
+   //! Give the type an STL name.
    typedef __iterator const_iterator;
    static const STR_ClassIdent identifier;
 
+   //! Not much to talk about.
    StrChunk() : ReferenceCounting(0), iter_storage(0)  { }
+   //! Not much to talk about.
    virtual ~StrChunk()                                 { }
 
    inline virtual int AreYouA(const ClassIdent &cid) const;
 
-   //: Number of octets would this chunk takes up.  May be deprecated.
+   //! Number of octets this chunk takes up.  May be deprecated.
    virtual unsigned int Length() const = 0;
-   //: Number of 'sub groups' make up this entire chunk?
-   virtual unsigned int NumSubGroups() const;
-   //: How many 'sub groups' make up the section of this chunk denoted by
-   //: extent.
-   virtual unsigned int NumSubGroups(const LinearExtent &extent) const = 0;
-   //: Fill a GroupVector with a list of all groups making up this chunk.
-   inline void SimpleFillGroupVec(GroupVector &vec);
-   //: Fill a GroupVector with a list of all groups making up this chunk.
-   // Fill a GroupVector with a list of all groups making up this chunk
-   // starting at index start_index in the GroupVector.
-   virtual void FillGroupVec(GroupVector &vec, unsigned int &start_index);
-   //: Fill a GroupVector with a list of groups in this chunk.
-   // Fill a GroupVector with a list of groups in this chunk falling within
-   // the section denoted by extent, starting at index start_index in the
-   // GroupVector.
-   virtual void FillGroupVec(const LinearExtent &extent,
-			     GroupVector &vec, unsigned int &start_index) = 0;
-   //: Drop any unused sub chunks from this StrChunk.  <strong>Largely
-   //: unimplemented and unsafe to use.  Interface may change.</strong>
-   // <p>This is a dummy function that checks the reference count first, then
-   // calls i_DropUnused.  If this chunk is referenced from more than one
-   // place a determination of which chunks are unused is impossible.</p>
-   inline void DropUnused(const LinearExtent &usedextent,
-			  KeepDir keepdir = KeepLeft);
-
-   //! Get an STL style const bidirectional iterator.
-   __iterator begin();
-   __iterator end();
+   
+   //@{
+   /**
+    * Get an STL style const bidirectional iterator.
+    *
+    * This iterator will range over all the octects of type U1Byte that are in
+    * the chunk.
+    */
+   const_iterator begin();
+   const_iterator end();
+   //@}
 
  protected:
+   //! See class Protocol
    virtual const ClassIdent *i_GetIdent() const        { return(&identifier); }
 
-   //: The actual DropUnused function that needs to be overloaded in derived
-   //: classes.
-   // This only really has meaning for compound StrChunks.  The keepdir
-   // parameter tells the function how to handle 0 length StrChunks.  See the
-   // documentation for KeepDir for more information.
-   virtual void i_DropUnused(const LinearExtent &usedextent,
-			     KeepDir keepdir) = 0;
-
-   //: Accept a ChunkVisitor, and maybe lead it through your children.
+   //! Accept a ChunkVisitor, and maybe lead it through your children.
    virtual void acceptVisitor(ChunkVisitor &visitor)
       throw(ChunkVisitor::halt_visitation) = 0;
 
+   //@{
+   /**
+    * These are helper functions so derived classes have limited access to
+    * ChunkVisitor methods.
+    */
    inline void call_visitStrChunk(ChunkVisitor &visitor,
                                   const StrChunkPtr &chunk)
       throw(ChunkVisitor::halt_visitation);
@@ -128,9 +120,10 @@ class StrChunk : public Object, public ReferenceCounting {
    inline void call_visitDataBlock(ChunkVisitor &visitor,
                                    void *start, size_t len)
       throw(ChunkVisitor::halt_visitation);
+   //@}
 
  private:
-   void *iter_storage;
+   void *iter_storage;  //!< A kludgey storage area so iterators can avoid constantly running a Visitor over the same StrChunk.
 };
 
 //------------------------inline functions for StrChunk------------------------
@@ -138,21 +131,6 @@ class StrChunk : public Object, public ReferenceCounting {
 inline int StrChunk::AreYouA(const ClassIdent &cid) const
 {
    return((identifier == cid) || Object::AreYouA(cid));
-}
-
-inline void StrChunk::SimpleFillGroupVec(GroupVector &vec)
-{
-   unsigned int i = 0;
-
-   FillGroupVec(vec, i);
-}
-
-inline void StrChunk::DropUnused(const LinearExtent &usedextent,
-				 KeepDir keepdir)
-{
-   if (NumReferences() <= 1) {
-      i_DropUnused(usedextent, keepdir);
-   }
 }
 
 inline void StrChunk::call_visitStrChunk(ChunkVisitor &visitor,
