1acf750d43c26b5df292f839f393f7e917866e92
Eric Hopper <hopper@omnifarious.org>
2001-02-20 02:14:44 +0000

    New class for holding local copies of member variables.  Like StackSwapper.

---
+++ StrMod/LocalCopy.cxx
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2001 by Eric M. Hopper <hopper@omnifarious.mn.org>
+ * 
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU Lesser General Public License as published
+ *     by the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* $Header$ */
+
+// For a log, see ChangeLog
+
+#ifdef __GNUG__
+#  pragma implementation "LocalCopy.h"
+#endif
+
+#include "StrMod/LocalCopy.h"
