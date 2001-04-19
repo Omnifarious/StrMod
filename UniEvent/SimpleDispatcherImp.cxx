2c098ff55fb21be59ec3c6d16e759ba66d2a3bed
Eric Hopper <hopper@omnifarious.org>
2001-04-19 19:40:50 +0000

    Changed from Perceps to doxygen comments.
    Complete overhaul to implement busy polling and only allow one 'on queue
    empty' event.

--- UniEvent/SimpleDispatcherImp.cxx
+++ UniEvent/SimpleDispatcherImp.cxx
@@ -20,6 +20,10 @@
 
 // Log can be found in ChangeLog
 
+#ifdef __GNUG__
+#  pragma implementation "SimpleDispatcher.h"
+#endif
+
 #include "UniEvent/SimpleDispatcher.h"
 #include "UniEvent/Event.h"
 #include "UniEvent/EventPtr.h"
@@ -30,18 +34,21 @@ typedef deque<UNIEvent *> EVListBase;
 
 class UNISimpleDispatcher::Imp {
  public:
-   class EVList : public EVListBase {
+   class EVList : private EVListBase {
     public:
       inline void addElement(UNIEvent *ev);
       inline bool qEmpty();
+      inline size_t size() const { return EVListBase::size(); }
+      inline void moveFrontTo(EVList &b);
+      inline UNIEvent *noref_pop_front();
 
       inline ~EVList();
    };
 
-   void flopQueues();
-
    EVList mainq_;
-   EVList onempty_;
+   EVList busypoll_;
+   size_t internalevnum_;
+   UNIEventPtr onempty_;
 };
 
 inline void UNISimpleDispatcher::Imp::EVList::addElement(UNIEvent *ev)
@@ -73,19 +80,27 @@ inline UNISimpleDispatcher::Imp::EVList::~EVList()
    }
 }
 
-void UNISimpleDispatcher::Imp::flopQueues()
+inline void UNISimpleDispatcher::Imp::EVList::moveFrontTo(EVList &b)
 {
-   while (!onempty_.qEmpty())
-   {
-      UNIEvent *ev = onempty_.front();
-      onempty_.pop_front();
-      mainq_.push_back(ev);
-   }
+   assert(size() > 0);
+   UNIEvent *ev = front();
+   pop_front();
+   b.push_back(ev);
+}
+
+//! Remove and return the front element without altering the reference count.
+inline UNIEvent *UNISimpleDispatcher::Imp::EVList::noref_pop_front()
+{
+   assert(size() > 0);
+   UNIEvent *ev = front();
+   pop_front();
+   return ev;
 }
 
 UNISimpleDispatcher::UNISimpleDispatcher()
      : imp_(*(new Imp)), stop_flag_(false)
 {
+   imp_.internalevnum_ = 0;
 }
 
 UNISimpleDispatcher::~UNISimpleDispatcher()
@@ -98,40 +113,147 @@ UNISimpleDispatcher::~UNISimpleDispatcher()
    delete &imp_;
 }
 
-void UNISimpleDispatcher::AddEvent(const UNIEventPtr &ev)
+void UNISimpleDispatcher::addEvent(const UNIEventPtr &ev)
 {
    imp_.mainq_.addElement(ev.GetPtr());
 }
 
-void UNISimpleDispatcher::DispatchEvents(unsigned int numevents,
+void UNISimpleDispatcher::addBusyPollEvent(const UNIEventPtr &ev)
+{
+   imp_.busypoll_.addElement(ev.GetPtr());
+}
+
+inline void UNISimpleDispatcher::i_DispatchEvent(Imp &imp,
+                                                 UNIDispatcher *enclosing)
+{
+   assert(!imp.mainq_.qEmpty());
+   assert(enclosing != 0);
+
+   UNIEvent *ev = imp.mainq_.noref_pop_front();
+   ev->triggerEvent(enclosing);
+   if (ev->NumReferences() > 0)
+   {
+      ev->DelReference();
+   }
+   if (ev->NumReferences() <= 0)
+   {
+      delete ev;
+   }
+}
+
+unsigned int UNISimpleDispatcher::i_dispatchNEvents(unsigned int n,
+                                                    bool checkbusypoll,
+                                                    UNIDispatcher *enclosing)
+{
+   assert(imp_.mainq_.size() >= n);
+   assert(enclosing != 0);
+
+   Imp &imp = imp_;
+   unsigned int i = n;
+   for (; (i > 0) && !stop_flag_; --i)
+   {
+      assert(!imp.mainq_.qEmpty() && imp.internalevnum_ > 0);
+      i_DispatchEvent(imp, enclosing);
+      if (checkbusypoll && ! imp.busypoll_.qEmpty())
+      {
+         break;
+      }
+   }
+   return n - i;
+}
+
+inline unsigned int
+UNISimpleDispatcher::checkEmptyBusy(Imp &imp, bool &checkbusy)
+{
+   assert(imp.internalevnum_ <= imp.mainq_.size());
+   unsigned int retval = imp.internalevnum_;
+   checkbusy = false;
+   if (imp.internalevnum_ == 0)
+   {
+      if (imp.mainq_.qEmpty())
+      {
+         if (imp.onempty_)  // mainq empty, and there's an 'onempty' event.
+         {
+            // Post 'onempty' event to mainq.
+            imp.mainq_.addElement(imp.onempty_.GetPtr());
+            imp.internalevnum_ = 1;
+            imp.onempty_.ReleasePtr();
+            retval = 1;
+         }
+      }
+      else  // mainq_ isn't empty, so must be full of external events.
+      {
+         if (! imp.busypoll_.qEmpty())  // There are busypoll events to post.
+         {
+            // Post an event from the buspoll list and record where the
+            // internal event is on mainq.
+            imp.busypoll_.moveFrontTo(imp.mainq_);
+            imp.internalevnum_ = imp.mainq_.size();
+         }
+         else
+         {
+            // Pretend this event is an internal event so we check again next
+            // time through loop and there isn't any special case handling.
+            imp.internalevnum_ = 1;
+            checkbusy = true;
+         }
+      }
+      retval = imp.mainq_.size();
+   }
+   return retval;
+}
+
+void UNISimpleDispatcher::dispatchNEvents(unsigned int numevents,
+                                          UNIDispatcher *enclosing)
+{
+   assert(numevents > 0);
+   assert(enclosing != 0);
+
+   Imp &imp = imp_;
+
+   while (numevents > 0 && !stop_flag_)
+   {
+      bool checkbusy = false;
+      unsigned int evchunk = checkEmptyBusy(imp, checkbusy);
+      if (evchunk <= 0)
+      {
+         break;
+      }
+      else
+      {
+         if (evchunk > numevents)
+         {
+            evchunk = numevents;
+         }
+         unsigned int dispatched = i_dispatchNEvents(evchunk, checkbusy,
+                                                     enclosing);
+         if (dispatched > imp.internalevnum_)
+         {
+            imp.internalevnum_ = 0;
+         }
+         else
+         {
+            imp.internalevnum_ -= dispatched;
+         }
+         numevents -= dispatched;
+      }
+   }
+}
+
+void UNISimpleDispatcher::dispatchEvents(unsigned int numevents,
 					 UNIDispatcher *enclosing)
 {
-   if (enclosing == 0) {
-      enclosing = this;
-   }
-
-   unsigned int i = numevents;
-
-   while (i && (!imp_.mainq_.qEmpty()
-		|| !imp_.onempty_.qEmpty()) && !stop_flag_)
+   if (numevents <= 0)
    {
-      if (imp_.mainq_.qEmpty())
+      return;
+   }
+   else
+   {
+      if (enclosing == 0)
       {
-	 imp_.flopQueues();
+         enclosing = this;
       }
-      UNIEvent *ev = imp_.mainq_.front();
-
-      imp_.mainq_.pop_front();
-      i--;
-      ev->TriggerEvent(enclosing);
-      if (ev->NumReferences() > 0)
-      {
-	    ev->DelReference();
-      }
-      if (ev->NumReferences() <= 0)
-      {
-	 delete ev;
-      }
+      dispatchNEvents(numevents, enclosing);
    }
    if (stop_flag_)
    {
@@ -139,41 +261,36 @@ void UNISimpleDispatcher::DispatchEvents(unsigned int numevents,
    }
 }
 
-void UNISimpleDispatcher::DispatchUntilEmpty(UNIDispatcher *enclosing)
+void UNISimpleDispatcher::dispatchUntilEmpty(UNIDispatcher *enclosing)
 {
-   if (enclosing == 0) {
+   if (enclosing == 0)
+   {
       enclosing = this;
    }
-
-   while ((!imp_.mainq_.qEmpty() || !imp_.onempty_.qEmpty()) && !stop_flag_)
+   while (!stop_flag_ && ((imp_.mainq_.size() > 0) || imp_.onempty_))
    {
-      if (imp_.mainq_.qEmpty())
-      {
-	 imp_.flopQueues();
-      }
-
-      UNIEvent *ev = imp_.mainq_.front();
-
-      imp_.mainq_.pop_front();
-      ev->TriggerEvent(enclosing);
-      if (ev->NumReferences() > 0) {
-	    ev->DelReference();
-      }
-      if (ev->NumReferences() <= 0) {
-	 delete ev;
-      }
+      dispatchNEvents(imp_.mainq_.size() * 2 + 1, enclosing);
    }
-   if (stop_flag_) {
+   if (stop_flag_)
+   {
       stop_flag_ = false;
    }
 }
 
-bool UNISimpleDispatcher::IsQueueEmpty() const
+bool UNISimpleDispatcher::isQueueEmpty() const
 {
    return(imp_.mainq_.qEmpty());
 }
 
-void UNISimpleDispatcher::onQueueEmpty(const UNIEventPtr &ev)
+bool UNISimpleDispatcher::onQueueEmpty(const UNIEventPtr &ev)
 {
-   imp_.onempty_.addElement(ev.GetPtr());
+   if (imp_.onempty_)
+   {
+      return false;
+   }
+   else
+   {
+      imp_.onempty_ = ev;
+      return true;
+   }
 }
