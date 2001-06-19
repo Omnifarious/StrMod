4cc0ee33cf01a55b9f0ed6d555f33d64af40a6ba
Eric Hopper <hopper@omnifarious.org>
2001-06-19 22:21:55 +0000

    Tagged id 10 as no longer being used by UNIXError as UNIXError has bowed out of the type id system.

--- UniEvent/UNEVT_ClassIdent.cxx
+++ UniEvent/UNEVT_ClassIdent.cxx
@@ -39,5 +39,5 @@ const UNEVT_ClassIdent UNEVT_ClassIdent::identifier(0UL);
  *   7UL  UNIXpollManager
  *   8UL  UNIXpollManager::PollEvent
  *   9UL  UNIXpollManagerImp
- *  10UL  UNIXError
+ *  10UL  <nothing, was UNIXError>
  */
