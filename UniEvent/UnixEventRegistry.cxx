d84bb769a00a1d726cc5551460599f97af421916
Eric Hopper <hopper@omnifarious.org>
2002-02-28 22:29:09 +0000

    Initial version.  Just the .cxx stub for an interface.

---
+++ UniEvent/UnixEventRegistry.cxx
@@ -0,0 +1,12 @@
+/* $Header$ */
+
+// $Log$
+// Revision 1.1  2002/02/28 22:29:09  hopper
+// Initial version.  Just the .cxx stub for an interface.
+//
+
+#ifdef __GNUG__
+#  pragma implementation "UnixEventRegistry.h"
+#endif
+
+#include "UniEvent/UnixEventRegistry.h"
