3f20a96300421ab80b0a51e79d5fd35c47257455
Eric Hopper <hopper@omnifarious.org>
2002-03-21 16:09:52 +0000

    Set up a new test for the timer portion of UnixEventPoll.

--- UniEvent/tests/testupoll.cxx
+++ UniEvent/tests/testupoll.cxx
@@ -4,6 +4,8 @@
 #include <UniEvent/Event.h>
 #include <UniEvent/EventPtr.h>
 #include <iostream>
+#include <exception>
+#include <stdexcept>
 #include <cstring>
 #include <unistd.h>
 #include <sys/types.h>
@@ -79,51 +81,133 @@ void SigEvent::triggerEvent(strmod::unievent::Dispatcher *dispatcher)
    cout.flush();
 }
 
+class TimerEvent : public strmod::unievent::Event
+{
+   typedef ::strmod::unievent::Timer::interval_t interval_t;
+   typedef ::strmod::unievent::Timer::absolute_t absolute_t;
+ public:
+   TimerEvent()
+        : created_(currentTime()), recur_(interval_t(0, 0)),
+          last_(created_), recurbase_(created_), ureg_(0)
+   {
+   }
+   TimerEvent(strmod::unievent::UnixEventPoll *ureg,
+              const interval_t &recurint)
+        : created_(currentTime()), recur_(recurint),
+          last_(created_), recurbase_(created_), ureg_(ureg)
+   {
+   }
+
+   virtual void triggerEvent(strmod::unievent::Dispatcher *dispatcher = 0);
+
+ private:
+   const absolute_t created_;
+   const interval_t recur_;
+   absolute_t last_;
+   absolute_t recurbase_;
+   strmod::unievent::UnixEventPoll * const ureg_;
+
+   static const absolute_t currentTime()
+   {
+      struct ::timeval tv;
+      ::gettimeofday(&tv, 0);
+      return absolute_t(tv.tv_sec, 0, tv.tv_usec * 1000U);
+   }
+   static double interval_to_double(const interval_t &intval)
+   {
+      return intval.seconds + double(intval.nanoseconds) / 1000000000U;
+   }
+};
+
+void TimerEvent::triggerEvent(strmod::unievent::Dispatcher *dispatcher)
+{
+   using std::cout;
+   using strmod::unievent::UnixEventRegistry;
+   const absolute_t curtime = currentTime();
+   const interval_t sincecreated = curtime - created_;
+   const interval_t sincelast = curtime - last_;
+   cout << "Got a timer event (" << this
+        << ") at " << interval_to_double(sincecreated)
+        << " since creation, and " << interval_to_double(sincelast)
+        << " since the last time.\n";
+   if ((recur_.seconds > 0) || (recur_.nanoseconds > 0))
+   {
+      cout << "Setting up another event in " << interval_to_double(recur_)
+           << " seconds.\n";
+      ureg_->postAt(recurbase_ + recur_, this);
+      recurbase_ = recurbase_ + recur_;
+   }
+   cout.flush();
+   last_ = curtime;
+}
+
 int main(int argc, const char *argv[])
 {
-   strmod::unievent::SimpleDispatcher dispatcher;
-   strmod::unievent::UnixEventPoll upoll(&dispatcher);
-   using ::std::cerr;
+   try {
+      strmod::unievent::SimpleDispatcher dispatcher;
+      strmod::unievent::UnixEventPoll upoll(&dispatcher);
+      using ::std::cerr;
 
-   using strmod::unievent::UnixEventRegistry;
-   upoll.printState(cerr);
-   const UnixEventRegistry::FDCondSet rdcond(UnixEventRegistry::FD_Readable);
-   upoll.registerFDCond(0, rdcond, new StdinEvent(&upoll, 0));
-   for (int i = 1; i < argc; ++i)
-   {
-      cerr << "Opening up \"" << argv[i] << "\" for reading.\n";
-      const int tempfd = open(argv[i], O_RDONLY | O_NONBLOCK);
-      cerr << "Got fd # " << tempfd << "\n";
-      if (tempfd >= 0)
+      using strmod::unievent::UnixEventRegistry;
+      upoll.printState(cerr);
+      const UnixEventRegistry::FDCondSet rdcond(UnixEventRegistry::FD_Readable);
+      upoll.registerFDCond(0, rdcond, new StdinEvent(&upoll, 0));
+      for (int i = 1; i < argc; ++i)
       {
-         upoll.registerFDCond(tempfd, rdcond, new StdinEvent(&upoll, tempfd));
+         cerr << "Opening up \"" << argv[i] << "\" for reading.\n";
+         const int tempfd = open(argv[i], O_RDONLY | O_NONBLOCK);
+         cerr << "Got fd # " << tempfd << "\n";
+         if (tempfd >= 0)
+         {
+            upoll.registerFDCond(tempfd, rdcond, new StdinEvent(&upoll, tempfd));
+         }
       }
-   }
-   upoll.onSignal(SIGINT, new SigEvent(&upoll, SIGINT));
-   upoll.onSignal(SIGINT, new SigEvent(&upoll, SIGINT, 5));
-   upoll.onSignal(SIGALRM, new SigEvent(&upoll, SIGALRM));
-   {
-      struct ::itimerval intval;
-      intval.it_interval.tv_sec = 2;
-      intval.it_interval.tv_usec = 500000;
-      intval.it_value.tv_sec = intval.it_interval.tv_sec;
-      intval.it_value.tv_usec = intval.it_interval.tv_usec;
-      ::setitimer(ITIMER_REAL, &intval, 0);
-   }
+      upoll.onSignal(SIGINT, new SigEvent(&upoll, SIGINT));
+      upoll.onSignal(SIGINT, new SigEvent(&upoll, SIGINT, 5));
+      upoll.postIn(strmod::unievent::Timer::interval_t(10, 0), new TimerEvent());
+      upoll.postIn(strmod::unievent::Timer::interval_t(3, 125000000U),
+                   new TimerEvent(&upoll,
+                                  strmod::unievent::Timer::interval_t(3, 125000000U)));
+//     upoll.onSignal(SIGALRM, new SigEvent(&upoll, SIGALRM));
+//     {
+//        struct ::itimerval intval;
+//        intval.it_interval.tv_sec = 2;
+//        intval.it_interval.tv_usec = 500000;
+//        intval.it_value.tv_sec = intval.it_interval.tv_sec;
+//        intval.it_value.tv_usec = intval.it_interval.tv_usec;
+//        ::setitimer(ITIMER_REAL, &intval, 0);
+//     }
 //   upoll.printState(std::cerr);
-   do {
-      if (dispatcher.isQueueEmpty())
-      {
-         upoll.printState(::std::cerr);
-         ::std::cerr << "\n";
-         upoll.doPoll(true);
+      do {
+         if (dispatcher.isQueueEmpty())
+         {
+            upoll.printState(::std::cerr);
+            ::std::cerr << "\n";
+            upoll.doPoll(true);
 //         upoll.printState(std::cerr);
-      }
-      else
-      {
+         }
+         else
+         {
 //         upoll.printState(std::cerr);
-         dispatcher.dispatchUntilEmpty();
+            dispatcher.dispatchUntilEmpty();
 //         upoll.printState(std::cerr);
-      }
-   } while (true);
+         }
+      } while (true);
+   }
+   catch (::std::range_error re)
+   {
+      ::std::cerr << "Died of a range_error: " << re.what() << "\n";
+   }
+   catch (::std::logic_error le)
+   {
+      ::std::cerr << "Died of a logic_error: " << le.what() << "\n";
+   }
+   catch (::std::runtime_error re)
+   {
+      ::std::cerr << "Died of a runtime_error: " << re.what() << "\n";
+   }
+   catch (::std::exception e)
+   {
+      ::std::cerr << "Died of an exception: " << e.what() << "\n";
+   }
 }
