497e82ddf6510b8393ec6e6a044a6169223bf022
Eric Hopper <hopper@omnifarious.org>
2002-03-22 23:31:43 +0000

    Fleshed out the class a bit.

--- UniEvent/RegistryDispatcherGlue.cxx
+++ UniEvent/RegistryDispatcherGlue.cxx
@@ -7,3 +7,22 @@
 #endif
 
 #include "UniEvent/RegistryDispatcherGlue.h"
+
+namespace strmod {
+namespace unievent {
+
+RegistryDispatcherGlue::RegistryDispatcherGlue(Dispatcher *disp,
+                                               UnixEventRegistry *ureg)
+     : disp_(disp), ureg_(ureg),
+       emptyev_(new EmptyEvent(this)), busyev_(new BusyEvent(this))
+{
+}
+
+RegistryDispatcherGlue::~RegistryDispatcherGlue()
+{
+   emptyev_->parentGone();
+   busyev_->parentGone();
+}
+
+} // namespace unievent
+} // namespace strmod
