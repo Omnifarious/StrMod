414b1c44422d52d970546ae074eaa833fe00e58b
Eric Hopper <hopper@omnifarious.org>
2002-03-27 13:18:00 +0000

    Fixed printState so type of argument would be more consisten.

--- LCore/LCore/Debugable.h
+++ LCore/LCore/Debugable.h
@@ -53,7 +53,7 @@ class Debugable : virtual public Protocol {
    /** This should print out as much of the internal state of a class as would
     * be needed to debug it properly.
     */
-   virtual void printState(std::ostream &os) const = 0;
+   virtual void printState(::std::ostream &os) const = 0;
 
  protected:
    virtual const ClassIdent *i_GetIdent() const         { return(&identifier); }
