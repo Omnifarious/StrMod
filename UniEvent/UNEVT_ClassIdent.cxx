8b73d3733da66fa56850e6dcf0e6f37a0c16d97f
Eric Hopper <hopper@omnifarious.org>
2002-03-27 12:46:00 +0000

    Documented that this will be cleared away.

--- UniEvent/UNEVT_ClassIdent.cxx
+++ UniEvent/UNEVT_ClassIdent.cxx
@@ -37,6 +37,8 @@ namespace unievent {
 /** \class UNEVT_ClassIdent UNEVT_ClassIdent.h UniEvent/UNEVT_ClassIdent.h
  * \brief An ignored blurb.
  *
+ * Use of the ClassIdent classes is being phased out in favor of RTTI.
+ *
  * Here is a list of currently used identifiers:
  * <pre>
  *   0UL  UNEVT_ClassIdent
