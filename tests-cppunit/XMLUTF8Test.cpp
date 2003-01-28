58e770d2be85a4ff6f6c4461c3f989841a876d4f
Eric Hopper <hopper@omnifarious.org>
2003-01-28 02:46:49 -0800

    [svn r60] tests-cppunit/XMLUTF8Test.cpp is now a viable test.

--- tests-cppunit/XMLUTF8Test.cpp
+++ tests-cppunit/XMLUTF8Test.cpp
@@ -15,6 +15,9 @@
 #include <stack>
 #include <vector>
 #include <stdexcept>
+#include <sstream>
+#include <iostream>
+#include <fstream>
 
 using ::strmod::xml::utf8::Lexer;
 using ::strmod::xml::utf8::Builder;
@@ -60,12 +63,14 @@ struct ElementData
 class TestBuilder : public Builder
 {
  public:
-   TestBuilder(const char *buf, size_t blen, size_t chnklen)
-        : buf_(buf), blen_(blen), blen2_(blen / 2), chnklen_(chnklen)
+   TestBuilder(const char *buf, size_t blen, size_t chnklen,
+               const string &goodparse, Lexer &lexer)
+        : buf_(buf), blen_(blen), blen2_(blen / 2), chnklen_(chnklen),
+          lexer_(lexer), goodparse_(goodparse)
    {
       CPPUNIT_ASSERT(blen % 2 == 0);
       {
-         unsigned int a = blen, b = chnklen;
+         unsigned int a = blen2_, b = chnklen;
          unsigned int rem = a % b;
          while (rem != 0)
          {
@@ -73,7 +78,7 @@ class TestBuilder : public Builder
             b = rem;
             rem = a % b;
          }
-         CPPUNIT_ASSERT(b == 1);
+         CPPUNIT_ASSERT((b == 1) || (blen2_ == chnklen));
       }
    }
 
@@ -93,6 +98,9 @@ class TestBuilder : public Builder
    const size_t blen2_;
    size_t chnklen_;
    elstack_t elements_;
+   ::std::ostringstream result_;
+   Lexer &lexer_;
+   const string goodparse_;
 
    const string buildString(const Position &begin, const Position &end);
    void elementFinished(const Position &celend);
@@ -109,7 +117,8 @@ void TestBuilder::addAttribute(const Position &attrbegin,
                                const Position &valend, const string &name)
 {
    CPPUNIT_ASSERT(elements_.size() > 0);
-   string literal(buildString(valbegin, valend));
+//   ::std::cerr << "Adding attribute: " << name << " to element: " << elements_.top().name_ << "\n";
+   string literal(buildString(attrbegin, attrend));
    elements_.top().attrlist_.push_back(
       AttributeData(name, buildString(valbegin, valend), literal,
                     literal.at(literal.size() - 1)));
@@ -123,7 +132,17 @@ void TestBuilder::endElementTag(const Position &selend, bool wasempty)
    if (wasempty)
    {
       el.datend_ = selend;
+//      ::std::cerr << "Ended " << el.name_ << "\n";
       elementFinished(selend);
+   }
+   else
+   {
+      if (el.name_ == "store")
+      {
+//         ::std::cerr << "Non whitespace OK!\n";
+         lexer_.setNonWSInElements(true);
+      }
+//      ::std::cerr << "Ended " << el.name_ << "\n";
    }
 }
 
@@ -135,217 +154,122 @@ void TestBuilder::closeElementTag(const Position &celbegin,
    ElementData &el = elements_.top();
    CPPUNIT_ASSERT(el.name_ == name);
    el.datend_ = celbegin;
+   if (el.name_ == "store")
+   {
+//      ::std::cerr << "Non whitespace not OK!\n";
+      lexer_.setNonWSInElements(false);
+   }
    elementFinished(celend);
+}
+
+const string TestBuilder::buildString(const Position &begin,
+                                      const Position &end)
+{
+   const unsigned int begpos =
+      (begin.bufhdl_.ulval_ * chnklen_ + begin.bufoffset_) % blen2_;
+   unsigned int endpos =
+      (end.bufhdl_.ulval_ * chnklen_ + end.bufoffset_) % blen2_;
+   if (begpos > endpos)
+   {
+      endpos += blen2_;
+   }
+   CPPUNIT_ASSERT(begpos <= endpos);
+   return string(buf_ + begpos, endpos - begpos);
+}
+
+void TestBuilder::elementFinished(const Position &celend)
+{
+   CPPUNIT_ASSERT(elements_.size() > 0);
+   {
+      ElementData &el = elements_.top();
+      const unsigned int level = elements_.size() - 1;
+      result_ << level << " !--> "
+              << el.name_ << " | "
+              << buildString(el.elbegin_, el.datbegin_) << "\n";
+      for (attrlist_t::iterator i = el.attrlist_.begin();
+           i != el.attrlist_.end();
+           ++i)
+      {
+         AttributeData &attr = *i;
+         result_ << level << " A--> "
+                 << attr.name_ << " | "
+                 << attr.delimiter_ << attr.value_ << attr.delimiter_ << " | "
+                 << attr.literal_ << "\n";
+      }
+      result_ << level << " V--> "
+              << buildString(el.datbegin_, el.datend_) << "\n";
+      result_ << level << " C--> "
+              << buildString(el.datend_, celend) << "\n\n";
+   }
+   elements_.pop();
+   if (elements_.size() == 0)
+   {
+      CPPUNIT_ASSERT(result_.str() == goodparse_);
+//       ::std::ofstream of("fred.out");
+//       of << result_.str();
+      string empty;
+      result_.str(empty);
+   }
+   else
+   {
+//      ::std::ofstream of("fred.out");
+//       ::std::cerr << result_.str();
+   }
 }
 
 }
 
 class XMLUTF8Test : public ::CppUnit::TestFixture
 {
-//   CPPUNIT_TEST_SUITE(XMLUTF8Test);
+   CPPUNIT_TEST_SUITE(XMLUTF8Test);
+   CPPUNIT_TEST(testSingleScan);
+   CPPUNIT_TEST(testManyChunkSizes);
+   CPPUNIT_TEST_SUITE_END();
  private:
    static const char S_xmlstr[383];
-};
+   static const char S_goodparse[1226];
 
-const char XMLUTF8Test::S_xmlstr[383] = "<fred> <went joe=\"'barney\"\nalley='\"kate'> <down> <to> <the> <street> </street> <br/> </the> <a time= 'then' ><store time='now'>The New French Bakery</store\f></a>\r</to> </down> </went>\t</fred><fred> <went joe=\"'barney\"\nalley='\"kate'> <down> <to> <the> <street> </street> <br/> </the> <a time= 'then' ><store time='now'>The New French Bakery</store\f></a>\r</to> </down> </went>\t</fred>";
-
-}
-}
-
-#if 0
-struct AttributeData
-{
-   ::std::string name_;
-   ::std::string value_;
-   ::std::string literal_;
-   char delimiter_;
-
-   AttributeData(const ::std::string &name, const ::std::string &value,
-                 const ::std::string &literal, char delimiter)
-        : name_(name), value_(value), literal_(literal), delimiter_(delimiter)
-   {
-   }
-};
-
-struct ElementData
-{
-   const ::std::string name_;
-   size_t elbegin_;
-   size_t datbegin_;
-   size_t datend_;
-   ::std::vector<AttributeData> attrlist_;
-
-   ElementData(const ::std::string &name, size_t elbegin)
-        : name_(name), elbegin_(elbegin), datbegin_(elbegin), datend_(elbegin)
-   {
-   }
-};
-
-class TestBuilder : public Builder
-{
  public:
-   TestBuilder(const char *buf, Lexer &lexer) : buf_(buf), lexer_(lexer) { }
-   virtual ~TestBuilder() { }
-
-   virtual void startElementTag(const Position &begin, const ::std::string &name)
-   {
-      elstack_.push(ElementData(name, begin.bufoffset_));
-   }
-   virtual void addAttribute(const Position &attrbegin, const Position &attrend,
-                             const Position &valbegin, const Position &valend,
-                             const ::std::string &name)
-   {
-      ::std::string literal(buf_ + attrbegin.bufoffset_,
-                            attrend.bufoffset_ - attrbegin.bufoffset_);
-      ::std::string value(buf_ + valbegin.bufoffset_,
-                          valend.bufoffset_ - valbegin.bufoffset_);
-      elstack_.top().attrlist_.push_back(AttributeData(name, value, literal, buf_[valend.bufoffset_]));
-   }
-   virtual void endElementTag(const Position &end, bool wasempty)
-   {
-      ElementData &eldata = elstack_.top();
-      eldata.datbegin_ = end.bufoffset_;
-//       ::std::cerr << "eldata.elbegin_ == " << eldata.elbegin_
-//                   << " && eldata.datbegin_ == " << eldata.datbegin_ << "\n";
-      ::std::string tag(buf_ + eldata.elbegin_,
-                        eldata.datbegin_ - eldata.elbegin_);
-//      ::std::cerr << "tag == \"" << tag << "\"\n";
-      for (size_t i = 1; i < elstack_.size(); ++i) {
-         cout << "   ";
-      }
-      cout << "<" << eldata.name_;
-      if (eldata.attrlist_.size() > 0)
-      {
-         typedef ::std::vector<AttributeData> attrs_t;
-         attrs_t &attrlist_ = eldata.attrlist_;
-         for (attrs_t::iterator i = attrlist_.begin(); i != attrlist_.end(); ++i)
-         {
-            cout << " " << i->name_ << "="
-                 << i->delimiter_ << i->value_ << i->delimiter_
-                 << " :=: [" << i->literal_ << "]";
-         }
-      }
-      if (wasempty) {
-         eldata.datend_ = end.bufoffset_;
-         cout << "/> :=: [" << tag << "]\n";
-//          cout.flush();
-         elstack_.pop();
-      } else {
-         cout << "> :=: [" << tag << "]\n";
-//          cout.flush();
-         if (eldata.name_ == "store")
-         {
-            lexer_.setNonWSInElements(true);
-         }
-      }
-   }
-   virtual void closeElementTag(const Position &begin,
-                                const Position &end, const ::std::string &name)
-   {
-      ElementData &eldata = elstack_.top();
-      eldata.datend_ = begin.bufoffset_;
-      ::std::string data = ::std::string(buf_ + eldata.datbegin_,
-                                         eldata.datend_ - eldata.datbegin_);
-      ::std::string tag = ::std::string(buf_ + begin.bufoffset_,
-                                        end.bufoffset_ - begin.bufoffset_);
-      if (name != eldata.name_) {
-         throw ::std::runtime_error("Parse error!\n");
-      }
-      if (name == "store")
-      {
-         lexer_.setNonWSInElements(false);
-      }
-      for (size_t i = 1; i < elstack_.size(); ++i) {
-         cout << "   ";
-      }
-      cout << "</" << eldata.name_ << ">"
-           << " :=: [" << tag << "] :=: [" << data << "]\n";
-      elstack_.pop();
-   }
-
- private:
-   const char * const buf_;
-   Lexer &lexer_;
-   ::std::stack<ElementData> elstack_;
+   void testSingleScan();
+   void testManyChunkSizes();
 };
 
-}
-}
-}
+CPPUNIT_TEST_SUITE_REGISTRATION(XMLUTF8Test);
 
-using strmod::tests::TestBuilder;
-
-int main()
+void XMLUTF8Test::testSingleScan()
 {
-   std::set_terminate (__gnu_cxx::__verbose_terminate_handler);
-   try {
-      const char * const xmlstr = "<fred> <went joe=\"'barney\"\nalley='\"kate'> <down> <to> <the> <street> </street> <br/> </the> <a time= 'then' ><store time='now'>The New French Bakery</store\f></a>\r</to> </down> </went>\t</fred>";
-      const char * const xml2str =
-"<fred>\n"
-"   <went>\n"
-"      <down>\n"
-"         <to>\n"
-"            <the>\n"
-"               <street>\n"
-"               </street>\n"
-"               <br/>\n"
-"            </the>\n"
-"         </to>\n"
-"      </down>\n"
-"   </went>\n"
-"</fred>\n";
-      Lexer lexer;
-      {
-         TestBuilder ts(xmlstr, lexer);
-         ::std::cout << "Parsing: [" << xmlstr << "]\n\n";
-         Builder::BufHandle bh;
-         bh.ulval_ = 0;
-         lexer.lex(xmlstr, ::strlen(xmlstr), bh, ts);
-      }
-      ::std::cout << "================================\n";
-      {
-         TestBuilder ts(xml2str, lexer);
-         ::std::cout << "Parsing: [" << xml2str << "]\n\n";
-         Builder::BufHandle bh;
-         bh.ulval_ = 0;
-         lexer.lex(xml2str, ::strlen(xml2str), bh, ts);
-      }
-      return 0;
-   } catch (::std::exception e) {
-      ::std::cerr << "Dying of an exception exception: [" << e.what() << "]\n";
-      throw;
-   } catch (...) {
-      ::std::cerr << "Dying of an exception!\n";
-      throw;
-   }
-   return 0;
+   Lexer lexer;
+   TestBuilder builder(S_xmlstr, sizeof(S_xmlstr) - 1, sizeof(S_xmlstr) / 2, S_goodparse, lexer);
+   Builder::BufHandle buf;
+   buf.ulval_ = 0;
+   lexer.lex(S_xmlstr, sizeof(S_xmlstr) / 2, buf, builder);
+   buf.ulval_ = 1;
+   lexer.lex(S_xmlstr, sizeof(S_xmlstr), buf, builder);
 }
 
-// $Log$
-// Revision 1.8  2003/01/10 02:29:49  hopper
-// Initial, dumb support for the multi-buffer enabled Lexer and Builder
-// interfaces.
-//
-// Revision 1.7  2003/01/09 22:48:32  hopper
-// Much farter along multiple buffer parsing.
-//
-// Revision 1.6  2002/12/11 21:55:41  hopper
-// It parses attributes now.  There's even a decent test for it.  :-)
-//
-// Revision 1.5  2002/12/11 18:52:02  hopper
-// More steps towards parsing attributes.
-//
-// Revision 1.4  2002/12/10 22:46:02  hopper
-// Renamed the XMLParserStrategy to the more appropriate XMLBuilder from
-// Design Patterns.
-//
-// Revision 1.3  2002/12/10 16:08:42  hopper
-// Preliminary changes to allow elements to have #PCDATA.
-//
-// Revision 1.2  2002/12/10 13:21:55  hopper
-// Moved log to the end, like it is for all the other files.
-//
-// Revision 1.1  2002/12/10 13:19:16  hopper
-// Some XML parsing classes I'm playing with until I get something that's
-// suitable for public consumption.
-#endif
+void XMLUTF8Test::testManyChunkSizes()
+{
+   for (unsigned int chnksize = 3;
+        chnksize < ((sizeof(S_xmlstr) / 2) - 1);
+        ++chnksize)
+   {
+      Lexer lexer;
+      TestBuilder builder(S_xmlstr, sizeof(S_xmlstr) - 1, chnksize, S_goodparse, lexer);
+      unsigned int bufnum = 0;
+      unsigned int bufpos = 0;
+      do {
+         Builder::BufHandle bhdl;
+         bhdl.ulval_ = bufnum;
+         lexer.lex(S_xmlstr + bufpos, chnksize, bhdl, builder);
+         bufpos = (bufpos + chnksize) % (sizeof(S_xmlstr) / 2);
+         ++bufnum;
+      } while (bufpos != 0);
+   }
+}
+
+const char XMLUTF8Test::S_xmlstr[383] = "<fred> <went joe=\"'barney\"\nalley='\"kate'> <down> <to> <the> <street> </street> <br/> </the> <a time= 'then' ><store time='now'>The New French Bakery</store\t></a>\r</to> </down> </went>\t</fred><fred> <went joe=\"'barney\"\nalley='\"kate'> <down> <to> <the> <street> </street> <br/> </the> <a time= 'then' ><store time='now'>The New French Bakery</store\t></a>\r</to> </down> </went>\t</fred>";
+
+const char XMLUTF8Test::S_goodparse[1226] = "5 !--> street | <street>\n5 V-->  \n5 C--> </street>\n\n5 !--> br | <br/>\n5 V--> \n5 C--> \n\n4 !--> the | <the>\n4 V-->  <street> </street> <br/> \n4 C--> </the>\n\n5 !--> store | <store time='now'>\n5 A--> time | 'now' | time='now'\n5 V--> The New French Bakery\n5 C--> </store\t>\n\n4 !--> a | <a time= 'then' >\n4 A--> time | 'then' | time= 'then'\n4 V--> <store time='now'>The New French Bakery</store\t>\n4 C--> </a>\n\n3 !--> to | <to>\n3 V-->  <the> <street> </street> <br/> </the> <a time= 'then' ><store time='now'>The New French Bakery</store\t></a>\r\n3 C--> </to>\n\n2 !--> down | <down>\n2 V-->  <to> <the> <street> </street> <br/> </the> <a time= 'then' ><store time='now'>The New French Bakery</store\t></a>\r</to> \n2 C--> </down>\n\n1 !--> went | <went joe=\"'barney\"\nalley='\"kate'>\n1 A--> joe | \"'barney\" | joe=\"'barney\"\n1 A--> alley | '\"kate' | alley='\"kate'\n1 V-->  <down> <to> <the> <street> </street> <br/> </the> <a time= 'then' ><store time='now'>The New French Bakery</store\t></a>\r</to> </down> \n1 C--> </went>\n\n0 !--> fred | <fred>\n0 V-->  <went joe=\"'barney\"\nalley='\"kate'> <down> <to> <the> <street> </street> <br/> </the> <a time= 'then' ><store time='now'>The New French Bakery</store\t></a>\r</to> </down> </went>\t\n0 C--> </fred>\n\n";
+
+}
+}
