0d25905c31b56d73f9a999346bb183c59ca38586
Eric Hopper <hopper@omnifarious.org>
2018-06-01 08:19:43 -0700

    A place for the compiler to put auto-generated functions.

---
+++ LCore/Debugable.cxx
@@ -0,0 +1,26 @@
+/* $Header$ */
+/*
+ * Copyright 1991-2010 Eric M. Hopper <hopper@omnifarious.org>
+ * 
+ *     This program is free software; you can redistribute it and/or modify it
+ *     under the terms of the GNU Lesser General Public License as published
+ *     by the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful, but
+ *     WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *     Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public
+ *     License along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+// For a log, see ./ChangeLog
+
+#ifdef __GNUG__
+#  pragma implementation "Debugable.h"
+#endif
+
+#include "LCore/Debugable.h"
